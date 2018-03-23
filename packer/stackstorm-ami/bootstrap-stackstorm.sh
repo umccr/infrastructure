9eecff5bf2d380277cee00726f0f77096d764b29
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-03-23 14:49:41 +1100

    Cosmetic refactoring
    Aggregate commands into fewer scripts. Add more tags to the created AMI.

--- packer/stackstorm-ami/bootstrap-stackstorm.sh
+++ packer/stackstorm-ami/bootstrap-stackstorm.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -e # make sure any failling command will fail the whole script
 
+##### Set up stackstorm docker compose
+echo "--------------------------------------------------------------------------------"
 echo "Setting up StackStorm..."
 sudo apt-get update
 sudo apt-get install -y \
@@ -9,38 +11,35 @@ sudo apt-get install -y \
 
 git clone https://github.com/umccr/st2-docker-umccr.git /opt/st2-docker-umccr
 
+echo "--------------------------------------------------------------------------------"
 echo "Preparing for docker-compose"
 cd /opt/st2-docker-umccr
 # switch to the development branch
+# TODO: switch back to master once stable
 git checkout development
 
+
+##### pre-load the required docker images into the AMI (makes first startup in production faster)
+# NOTE: requires the local compose file, as the production env is not available
+cp docker-compose.local.yml docker-compose.yml
+make env
+docker-compose pull -q --parallel
+
+# make sure the production compose file is in place!
 cp docker-compose.prod.yml docker-compose.yml
 
 
-# make env
-# # NOTE: we have to initialise with a local setup, as the production environment
-# #       is not available. We switch back later for production...
-#
-# cp docker-compose.local.yml docker-compose.yml
-# cat docker-compose.yml
-#
-# docker pull umccr/stackstorm:latest
-# sleep 10
-#
-# echo "Installing packs..."
-# docker-compose up -d
-# sleep 10 # otherwise we may get connection errors on stackstorm API side
-# echo "Installing Arteria pack..."
-# docker-compose exec -T stackstorm st2 pack install https://github.com/umccr/arteria-packs.git
-# echo "Installing UMCCR pack..."
-# docker-compose exec -T stackstorm st2 pack install https://github.com/umccr/stackstorm-umccr.git
-# echo "Installing Ansible pack..."
-# docker-compose exec -T stackstorm st2 pack install https://github.com/umccr/stackstorm-ansible.git
-# echo "Installing st2 pack..."
-# docker-compose exec -T stackstorm st2 pack install https://github.com/StackStorm-Exchange/stackstorm-st2.git
-# echo "Installing PCGR pack..."
-# docker-compose exec -T stackstorm st2 pack install https://github.com/umccr/stackstorm-pcgr.git
-# echo "Finishing up..."
-# docker-compose down -v
-#
-# cp docker-compose.prod.yml docker-compose.yml
+echo "--------------------------------------------------------------------------------"
+echo "Add the docker-compose-up.sh start script"
+tee docker-compose-up.sh << END
+docker-compose up -d mongo
+docker-compose up -d redis
+docker-compose up -d postgres
+docker-compose up -d rabbitmq
+docker-compose up -d nginx-web
+docker-compose up -d nginx-gen
+docker-compose up -d nginx-letsencrypt
+docker-compose up -d
+END
+
+chmod +x docker-compose-up.sh
--- packer/stackstorm-ami/common-setup.sh
+++ packer/stackstorm-ami/common-setup.sh
@@ -1,12 +1,28 @@
-#!/bin/sh
-
+#!/bin/bash
 sudo apt-get update
 
 echo "--------------------------------------------------------------------------------"
+echo "Set timezone"
+# set to Melbourne local time
+sudo rm /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Australia/Melbourne /etc/localtime
+sudo ls -al /etc/localtime
+
+
+
+echo "--------------------------------------------------------------------------------"
+echo "Install awscli"
+sudo apt-get install -y python-pip
+pip install awscli --upgrade
+
+# TODO: see if we can set this via IAM profiles
+export AWS_DEFAULT_REGION=ap-southeast-2
+
+
+
+echo "--------------------------------------------------------------------------------"
 echo "Installing s3fs"
-##### s3fs
 # https://github.com/s3fs-fuse/s3fs-fuse
-# used to mount the S3 bucket with the st2 config to the instance
 sudo apt-get install -y automake autotools-dev fuse g++ git libcurl4-gnutls-dev libfuse-dev libssl-dev libxml2-dev make pkg-config
 
 cd /tmp/
@@ -17,24 +33,15 @@ cd s3fs-fuse
 make
 sudo make install
 
-echo "--------------------------------------------------------------------------------"
 echo "Configuring s3fs"
 
 echo "user_allow_other" | sudo tee -a /etc/fuse.conf
 
 
-#
+
+# echo "--------------------------------------------------------------------------------"
+# echo "Installing rexray"
 # ##### REX-Ray (not needed for the docker plugins)
 # # https://rexray.readthedocs.io/en/stable/user-guide/installation/
 # cd /tmp/
 # curl -sSL https://rexray.io/install | sh -s -- stable
-
-
-echo "--------------------------------------------------------------------------------"
-echo "Install awscli"
-
-sudo apt-get install -y python-pip
-pip install awscli --upgrade
-
-# TODO: see if we can set this via IAM profiles
-export AWS_DEFAULT_REGION=ap-southeast-2

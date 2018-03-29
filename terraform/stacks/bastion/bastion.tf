e775e7cf9103b4f746a1751004b44ae922503c28
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-03-29 13:34:20 +1100

    Add no-mfa group and policy

--- terraform/stacks/bastion/bastion.tf
+++ terraform/stacks/bastion/bastion.tf
@@ -12,28 +12,52 @@ provider "aws" {
 }
 
 
-# create a user with minimal permissions
+# create a users with minimal permissions
 module "florian_user" {
   source = "../../modules/iam_user/secure_user"
   username = "florian"
   pgp_key = "${var.pgp_key}"
 }
-data "template_file" "get_user_policy" {
+data "template_file" "get_user_florian_policy" {
     template = "${file("policies/get_user.json")}"
     vars {
         user_arn = "${module.florian_user.arn}"
     }
 }
-resource "aws_iam_policy" "get_user_policy" {
-  name   = "get_user_policy"
+resource "aws_iam_policy" "get_user_florian_policy" {
+  name   = "get_user_florian_policy"
   path   = "/"
-  policy = "${data.template_file.get_user_policy.rendered}"
+  policy = "${data.template_file.get_user_florian_policy.rendered}"
 }
 resource "aws_iam_policy_attachment" "get_user_policy_florian_attachment" {
     name       = "get_user_policy_florian_attachment"
-    policy_arn = "${aws_iam_policy.get_user_policy.arn}"
+    policy_arn = "${aws_iam_policy.get_user_florian_policy.arn}"
     groups     = []
     users      = [ "${module.florian_user.username}" ]
+    roles      = []
+}
+
+module "travis_user" {
+  source = "../../modules/iam_user/secure_user"
+  username = "travis"
+  pgp_key = "${var.pgp_key}"
+}
+data "template_file" "get_user_travis_policy" {
+    template = "${file("policies/get_user.json")}"
+    vars {
+        user_arn = "${module.travis_user.arn}"
+    }
+}
+resource "aws_iam_policy" "get_user_travis_policy" {
+  name   = "get_user_travis_policy"
+  path   = "/"
+  policy = "${data.template_file.get_user_travis_policy.rendered}"
+}
+resource "aws_iam_policy_attachment" "get_user_policy_travis_attachment" {
+    name       = "get_user_policy_travis_attachment"
+    policy_arn = "${aws_iam_policy.get_user_travis_policy.arn}"
+    groups     = []
+    users      = [ "${module.travis_user.username}" ]
     roles      = []
 }
 
@@ -42,15 +66,29 @@ resource "aws_iam_group" "ops_admins_prod" {
   name = "ops_admins_prod"
 }
 
+resource "aws_iam_group" "ops_admins_no_mfa_prod" {
+  name = "ops_admins_no_mfa_prod"
+}
+
 # assign members to groups
 resource "aws_iam_group_membership" "ops_admins_prod" {
-  name = "ops_admins_group_membership"
+  name = "ops_admins_prod_group_membership"
 
   users = [
     "${module.florian_user.username}"
   ]
 
   group = "${aws_iam_group.ops_admins_prod.name}"
+}
+
+resource "aws_iam_group_membership" "ops_admins_no_mfa_prod" {
+  name = "ops_admins_no_mfa_prod_group_membership"
+
+  users = [
+    "${module.travis_user.username}"
+  ]
+
+  group = "${aws_iam_group.ops_admins_no_mfa_prod.name}"
 }
 
 # define the assume role policy and define who can use it
@@ -69,6 +107,25 @@ resource "aws_iam_policy_attachment" "ops_admins_assume_ops_admin_role_attachmen
     name       = "ec2_policy_to_automation_role_attachment"
     policy_arn = "${aws_iam_policy.assume_ops_admin_role_policy.arn}"
     groups     = [ "${aws_iam_group.ops_admins_prod.name}" ]
+    users      = []
+    roles      = []
+}
+
+data "template_file" "assume_ops_admin_no_mfa_role_policy" {
+    template = "${file("policies/assume_role_no_mfa.json")}"
+    vars {
+        role_arn = "arn:aws:iam::472057503814:role/ops-admin"
+    }
+}
+resource "aws_iam_policy" "assume_ops_admin_no_mfa_role_policy" {
+  name   = "assume_ops_admin_no_mfa_role_policy"
+  path   = "/"
+  policy = "${data.template_file.assume_ops_admin_no_mfa_role_policy.rendered}"
+}
+resource "aws_iam_policy_attachment" "ops_admins_no_mfa_assume_ops_admin_no_mfa_role_attachment" {
+    name       = "ops_admins_no_mfa_assume_ops_admin_no_mfa_role_attachment"
+    policy_arn = "${aws_iam_policy.assume_ops_admin_no_mfa_role_policy.arn}"
+    groups     = [ "${aws_iam_group.ops_admins_no_mfa_prod.name}" ]
     users      = []
     roles      = []
 }
--- terraform/stacks/bastion/outputs.tf
+++ terraform/stacks/bastion/outputs.tf
@@ -7,3 +7,13 @@ output "florian_access_key" {
 output "florian_secret_access_key" {
   value = "${module.florian_user.encrypted_secret_access_key}"
 }
+
+output "travis_username" {
+  value = "${module.travis_user.username}"
+}
+output "travis_access_key" {
+  value = "${module.travis_user.access_key}"
+}
+output "travis_secret_access_key" {
+  value = "${module.travis_user.encrypted_secret_access_key}"
+}

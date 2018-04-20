53c019bfaa06547ddacbebee5ea94084c793442f
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-04-20 12:48:58 +1000

    Enable DataDog agent

--- terraform/stacks/stackstorm_prod/main.tf
+++ terraform/stacks/stackstorm_prod/main.tf
@@ -20,7 +20,7 @@ module "stackstorm" {
   stackstorm_sub_domain         = "stackstorm"
   stackstorm_data_volume_name   = "stackstorm-data-prod" # NOTE: volume must exist
   stackstorm_docker_volume_name = "stackstorm-docker-volumes-prod" # NOTE: volume must exist
-  # st2_hostname                  = "umccr-stackstorm-prod" # NOTE: if empty no DataDog agent is started
-  st2_hostname                  = ""
+  st2_hostname                  = "umccr-stackstorm-prod" # NOTE: if empty no DataDog agent is started
+  # st2_hostname                  = ""
   # ami_filters                   = "${var.ami_filters}" # can be used to overwrite the default AMI lookup
 }

087ae457a0cf81e2eeed2f6e4be794d161c95dd6
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-04-26 13:54:44 +1000

    Use test instead of prod for testing

--- terraform/stacks/stackstorm_prod/main.tf
+++ terraform/stacks/stackstorm_prod/main.tf
@@ -1,26 +1,35 @@
 terraform {
   backend "s3" {
-    bucket  = "umccr-terraform-prod"
+    bucket  = "umccr-terraform-states"
     key     = "stackstorm/terraform.tfstate"
     region  = "ap-southeast-2"
   }
 }
 
+variable "workspace_iam_roles" {
+  default = {
+    prod = "arn:aws:iam::472057503814:role/ops-admin"
+  }
+}
+
 provider "aws" {
-  region  = "ap-southeast-2"
+  region      = "ap-southeast-2"
+  assume_role {
+    role_arn     = "${var.workspace_iam_roles[terraform.workspace]}"
+  }
 }
 
 
 module "stackstorm" {
   source                        = "../../modules/stackstorm"
   instance_type                 = "t2.medium"
-  name_suffix                   = "_prod"
+  name_suffix                   = "_test"
   availability_zone             = "ap-southeast-2a"
-  root_domain                   = "prod.umccr.org"
+  root_domain                   = "test.umccr.org"
   stackstorm_sub_domain         = "stackstorm"
-  stackstorm_data_volume_name   = "stackstorm-data-prod" # NOTE: volume must exist
-  stackstorm_docker_volume_name = "stackstorm-docker-volumes-prod" # NOTE: volume must exist
-  st2_hostname                  = "umccr-stackstorm-prod" # NOTE: if empty no DataDog agent is started
-  # st2_hostname                  = ""
+  stackstorm_data_volume_name   = "stackstorm-data-test" # NOTE: volume must exist
+  stackstorm_docker_volume_name = "stackstorm-docker-volumes-test" # NOTE: volume must exist
+  # st2_hostname                  = "umccr-stackstorm-prod" # NOTE: if empty no DataDog agent is started
+  st2_hostname                  = ""
   # ami_filters                   = "${var.ami_filters}" # can be used to overwrite the default AMI lookup
 }

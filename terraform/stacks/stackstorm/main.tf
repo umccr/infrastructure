870617c069c9fe5da2f33351da42441c886fe3a7
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-04-30 10:31:00 +1000

    Update stackstorm stack for dev

--- terraform/stacks/stackstorm/main.tf
+++ terraform/stacks/stackstorm/main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket  = "umccr-terraform-prod"
+    bucket  = "umccr-terraform-dev"
     key     = "stackstorm/terraform.tfstate"
     region  = "ap-southeast-2"
   }
@@ -14,13 +14,13 @@ provider "aws" {
 module "stackstorm" {
   source                        = "../../modules/stackstorm"
   instance_type                 = "t2.medium"
-  name_suffix                   = "_prod"
+  name_suffix                   = "_dev"
   availability_zone             = "ap-southeast-2a"
-  root_domain                   = "prod.umccr.org"
+  root_domain                   = "dev.umccr.org"
   stackstorm_sub_domain         = "stackstorm"
-  stackstorm_data_volume_name   = "stackstorm-data-prod" # NOTE: volume must exist
-  stackstorm_docker_volume_name = "stackstorm-docker-volumes-prod" # NOTE: volume must exist
-  st2_hostname                  = "umccr-stackstorm-prod" # NOTE: if empty no DataDog agent is started
+  stackstorm_data_volume_name   = "stackstorm-data-dev" # NOTE: volume must exist
+  stackstorm_docker_volume_name = "stackstorm-docker-volumes-dev" # NOTE: volume must exist
+  st2_hostname                  = "umccr-stackstorm-dev" # NOTE: if empty no DataDog agent is started
   # st2_hostname                  = ""
   # ami_filters                   = "${var.ami_filters}" # can be used to overwrite the default AMI lookup
 }

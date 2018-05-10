1d21cfa8c2da7bf72ea654f034a3c81a9abc0eee
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-05-10 15:56:07 +1000

    Script improvements

--- scripts/vault-env-setup.sh
+++ scripts/vault-env-setup.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
+MAX_TTL='12h'
+RENEWAL_PERIOD='60m'
+
 after_script(){
   echo "name: $AWS_ACCOUNT_NAME"
   # build the Vault server address based on the AWS account name
   if [ ! $AWS_ACCOUNT_NAME = "" ]; then
     echo "Using AWS account: $AWS_ACCOUNT_NAME"
   else
-    echo "Could not find AWS account name. Make sure you called assume-role."
+    echo "Could not find AWS account name. Make sure AWS_ACCOUNT_NAME is set."
     return
   fi
 
-
-  if [ $AWS_ACCOUNT_NAME = "prod" ]; then
-    export VAULT_ADDR='https://vault.prod.umccr.org:8200'
-  elif [ $AWS_ACCOUNT_NAME = "dev" ]; then
-    export VAULT_ADDR='https://vault.dev.umccr.org:8200'
-  else
-    echo "Unrecognised or unset AWS account name. Could not create Vault server address."
-    return
-  fi
+  export VAULT_ADDR="https://vault.$AWS_ACCOUNT_NAME.umccr.org:8200"
 
   # check if there is already a Vault token
   if [ ! $VAULT_TOKEN = "" ]; then
@@ -32,7 +27,7 @@ after_script(){
     echo "Renewal unsuccessful. Trying to request a new token..."
   fi
 
-  # if GITHUB_TOKEN is not set, it will ask for it
+  # if GITHUB_TOKEN is not set, the vault login will ask for it
   echo "Attempting a Vault login"
   vault login -method=github token=$GITHUB_TOKEN
   if [ $? != 0 ]; then
@@ -41,12 +36,10 @@ after_script(){
   fi
 
   echo "Requesting Vault access token..."
-  vault_token=$(vault token create -explicit-max-ttl=30m -period=10m --format=json | jq -r .auth.client_token)
+  vault_token=$(vault token create -explicit-max-ttl=$MAX_TTL -period=$RENEWAL_PERIOD --format=json | jq -r .auth.client_token)
   if [ $vault_token = "" ]; then
     echo "ERROR requesting Vault token"
   fi
-  echo "Token: $vault_token"
-  # eval $(echo export VAULT_TOKEN="$vault_token")
   export VAULT_TOKEN="$vault_token"
   echo "Vault access token successful retrieved. Session envars exported."
 }

b42b53c4de298018a93721f9ef94a5d5adefda93
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-06-15 16:34:00 +1000

    Update user Oliver with keybase ID

--- terraform/stacks/bastion/outputs.tf
+++ terraform/stacks/bastion/outputs.tf
@@ -18,6 +18,16 @@ output "brainstorm_secret_access_key" {
   value = "${module.brainstorm_user.encrypted_secret_access_key}"
 }
 
+output "oliver_username" {
+  value = "${module.oliver_user.username}"
+}
+output "oliver_access_key" {
+  value = "${module.oliver_user.access_key}"
+}
+output "oliver_secret_access_key" {
+  value = "${module.oliver_user.encrypted_secret_access_key}"
+}
+
 output "packer_username" {
   value = "${module.packer_user.username}"
 }

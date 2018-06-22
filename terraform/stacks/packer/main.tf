8abe69e9e1a075bb5efa9c55e144fb4c0d1d4fbb
Roman Valls Guimera <brainstorm@nopcode.org>
2018-06-22 13:53:26 +1000

    Fix attachment to the umccrise policy/role/instance_profile

--- terraform/stacks/packer/main.tf
+++ terraform/stacks/packer/main.tf
@@ -28,7 +28,7 @@ resource "aws_iam_instance_profile" "new_packer_instance_profile" {
 resource "aws_iam_role" "packer_umccrise_role" {
   name               = "packer_umccrise_role"
   path               = "/"
-  assume_role_policy = "${file("${path.module}/policies/packer_umccrise_role.json")}"
+  assume_role_policy = "${file("${path.module}/policies/assume_packer_role.json")}"
 }
 
 # packer_instance_profile_umccrise: permissions passed to the packer created AWS EC2 instance for uccrise
@@ -49,6 +49,20 @@ resource "aws_iam_policy_attachment" "packer_ec2_policy_to_packer_role_attachmen
   groups     = []
   users      = []
   roles      = ["${aws_iam_role.packer_role.name}"]
+}
+
+resource "aws_iam_policy" "packer_umccrise" {
+  name   = "packer_umccrise"
+  path   = "/"
+  policy = "${file("${path.module}/policies/packer_umccrise_role.json")}"
+}
+
+resource "aws_iam_policy_attachment" "packer_umccrise_policy_to_packer_role_attachment" {
+  name       = "packer_umccrise_policy_to_packer_role_attachment"
+  policy_arn = "${aws_iam_policy.packer_umccrise.arn}"
+  groups     = []
+  users      = []
+  roles      = ["${aws_iam_role.packer_umccrise_role.name}"]
 }
 
 resource "aws_iam_policy" "packer_spotfleet" {

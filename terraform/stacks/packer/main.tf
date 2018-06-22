712a83afd930515604207e9e38a9b0db14f02123
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-06-22 12:50:19 +1000

    Add instance profile for packer umccrise

--- terraform/stacks/packer/main.tf
+++ terraform/stacks/packer/main.tf
@@ -1,28 +1,41 @@
 terraform {
   backend "s3" {
-    bucket  = "umccr-terraform-states"
-    key     = "packer/terraform.tfstate"
-    region  = "ap-southeast-2"
+    bucket         = "umccr-terraform-states"
+    key            = "packer/terraform.tfstate"
+    region         = "ap-southeast-2"
     dynamodb_table = "terraform-state-lock"
   }
 }
 
 provider "aws" {
-  region  = "ap-southeast-2"
+  region = "ap-southeast-2"
 }
 
-
+# packer_role: used to execute packer builder (and launch the builder EC2 instance)
 resource "aws_iam_role" "packer_role" {
   name               = "packer_role"
   path               = "/"
   assume_role_policy = "${file("${path.module}/policies/assume_packer_role.json")}"
 }
 
+# packer_instance_profile: permissions passed to the packer created AWS EC2 instance
 resource "aws_iam_instance_profile" "new_packer_instance_profile" {
-  name  = "new_packer_instance_profile"
+  name = "new_packer_instance_profile"
   role = "${aws_iam_role.packer_role.name}"
 }
 
+# packer_umccrise_role: role to grant access to resources needed by umccrise volume builder
+resource "aws_iam_role" "packer_umccrise_role" {
+  name               = "packer_umccrise_role"
+  path               = "/"
+  assume_role_policy = "${file("${path.module}/policies/packer_umccrise_role.json")}"
+}
+
+# packer_instance_profile_umccrise: permissions passed to the packer created AWS EC2 instance for uccrise
+resource "aws_iam_instance_profile" "packer_instance_profile_umccrise" {
+  name = "packer_instance_profile_umccrise"
+  role = "${aws_iam_role.packer_umccrise_role.name}"
+}
 
 resource "aws_iam_policy" "packer_ec2" {
   name   = "packer_ec2"
@@ -31,21 +44,22 @@ resource "aws_iam_policy" "packer_ec2" {
 }
 
 resource "aws_iam_policy_attachment" "packer_ec2_policy_to_packer_role_attachment" {
-    name       = "packer_ec2_policy_to_packer_role_attachment"
-    policy_arn = "${aws_iam_policy.packer_ec2.arn}"
-    groups     = []
-    users      = []
-    roles      = [ "${aws_iam_role.packer_role.name}" ]
+  name       = "packer_ec2_policy_to_packer_role_attachment"
+  policy_arn = "${aws_iam_policy.packer_ec2.arn}"
+  groups     = []
+  users      = []
+  roles      = ["${aws_iam_role.packer_role.name}"]
 }
 
 resource "aws_iam_policy" "packer_spotfleet" {
   path   = "/"
   policy = "${file("${path.module}/policies/AmazonEC2SpotFleetTaggingRole.json")}"
 }
+
 resource "aws_iam_policy_attachment" "spot_policy_to_packer_role_attachment" {
-    name       = "spot_policy_to_packer_role_attachment"
-    policy_arn = "${aws_iam_policy.packer_spotfleet.arn}"
-    groups     = []
-    users      = []
-    roles      = [ "${aws_iam_role.packer_role.name}" ]
+  name       = "spot_policy_to_packer_role_attachment"
+  policy_arn = "${aws_iam_policy.packer_spotfleet.arn}"
+  groups     = []
+  users      = []
+  roles      = ["${aws_iam_role.packer_role.name}"]
 }

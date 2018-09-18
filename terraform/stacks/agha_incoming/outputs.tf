32e711f127e0a7414f38e9a4d54facacd8db0f3a
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-09-18 14:12:21 +1000

    Add instance public DNS to output in agha_incoming stack

--- terraform/stacks/agha_incoming/outputs.tf
+++ terraform/stacks/agha_incoming/outputs.tf
@@ -5,3 +5,11 @@ output "workspace" {
 output "instance_tags" {
   value = "${jsonencode(var.workspace_instance_tags[terraform.workspace])}"
 }
+
+output "instance_public_dns" {
+  value = "${aws_spot_instance_request.stackstorm_instance.public_dns}"
+}
+
+output "instance_public_ip" {
+  value = "${aws_spot_instance_request.stackstorm_instance.public_ip}"
+}

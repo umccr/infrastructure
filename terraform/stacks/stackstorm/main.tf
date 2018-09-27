cd9588b46694576f81bcb8a85fac6a3310843a11
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-09-27 15:18:39 +1000

    Add required_version

--- terraform/stacks/stackstorm/main.tf
+++ terraform/stacks/stackstorm/main.tf
@@ -1,16 +1,18 @@
 terraform {
+  required_version = "~> 0.11.6"
+
   backend "s3" {
     # AWS access credentials are retrieved from env variables
-    bucket  = "umccr-terraform-states"
-    key     = "stackstorm/terraform.tfstate"
-    region  = "ap-southeast-2"
+    bucket         = "umccr-terraform-states"
+    key            = "stackstorm/terraform.tfstate"
+    region         = "ap-southeast-2"
     dynamodb_table = "terraform-state-lock"
   }
 }
 
 provider "aws" {
   # AWS access credentials are retrieved from env variables
-  region      = "ap-southeast-2"
+  region = "ap-southeast-2"
 }
 
 provider "vault" {
@@ -21,7 +23,6 @@ data "vault_generic_secret" "datadog" {
   path = "kv/datadog"
 }
 
-
 module "stackstorm" {
   # NOTE: the source cannot be interpolated, so we can't use a variable here and have to keep the difference bwtween production and developmment in a branch
   source                        = "git::git@github.com:umccr/infrastructure.git//terraform/modules/stackstorm?ref=0.1.0"
@@ -31,9 +32,10 @@ module "stackstorm" {
   availability_zone             = "ap-southeast-2a"
   root_domain                   = "${var.workspace_zone_domain[terraform.workspace]}"
   stackstorm_sub_domain         = "stackstorm"
-  stackstorm_data_volume_name   = "${var.workspace_st_data_volume[terraform.workspace]}" # NOTE: volume must exist
-  stackstorm_docker_volume_name = "${var.workspace_st_docker_volume[terraform.workspace]}" # NOTE: volume must exist
+  stackstorm_data_volume_name   = "${var.workspace_st_data_volume[terraform.workspace]}"                                 # NOTE: volume must exist
+  stackstorm_docker_volume_name = "${var.workspace_st_docker_volume[terraform.workspace]}"                               # NOTE: volume must exist
   st2_hostname                  = "${var.workspace_dd_hostname[terraform.workspace]}"
   datadog_apikey                = "${data.vault_generic_secret.datadog.data["api-key"]}"
+
   # ami_filters                   = "${var.ami_filters}" # can be used to overwrite the default AMI lookup
 }

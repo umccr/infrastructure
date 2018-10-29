a01d19ef1dd6289229bc38e0c41d8248cb0c3911
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2018-10-30 08:41:08 +1100

    Choose lambda according to dev/prod

--- scripts/samplesheet-monitor.sh
+++ scripts/samplesheet-monitor.sh
@@ -4,6 +4,9 @@ set -eo pipefail
 TARGET_FILE="SampleSheet.csv"
 MONITORED_DIR_DEV="/storage/shared/dev/Baymax"
 MONITORED_DIR_PROD="/storage/shared/raw/Baymax"
+LAMBDA_DEV="bootstrap_slack_lambda_dev"
+LAMBDA_PROD="bootstrap_slack_lambda_prod"
+
 
 # require env variable DEPLOY_ENV to be set
 if test -z "$DEPLOY_ENV"; then
@@ -14,8 +17,10 @@ fi
 # if the value of DEPLOY_ENV is anything else than 'prod' we default to the development setup
 if test "$DEPLOY_ENV" = "prod"; then
     DIR="$MONITORED_DIR_PROD"
+    LAMBDA="$LAMBDA_PROD"
 else
     DIR="$MONITORED_DIR_DEV"
+    LAMBDA="$LAMBDA_DEV"
 fi
 
 inotifywait -m -r --exclude "[^c][^s][^v]$" $DIR -e create -e moved_to |
@@ -24,6 +29,6 @@ inotifywait -m -r --exclude "[^c][^s][^v]$" $DIR -e create -e moved_to |
           whoami
           echo "$DEPLOY_ENV"
           source $HOME/.bashrc
-          AWS_PROFILE=sample_monitor aws lambda invoke --invocation-type RequestResponse --function-name bootstrap_slack_lambda_dev --region ap-southeast-2 --payload "{ \"topic\": \"Incoming run monitor\", \"title\": \"New runfolder detected\", \"message\": \"$path\"}" /dev/null
+          AWS_PROFILE=sample_monitor aws lambda invoke --invocation-type RequestResponse --function-name "$LAMBDA" --region ap-southeast-2 --payload "{ \"topic\": \"Incoming run monitor\", \"title\": \"New runfolder detected\", \"message\": \"$path\"}" /dev/null
         fi
     done

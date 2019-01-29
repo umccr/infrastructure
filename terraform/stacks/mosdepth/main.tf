cb41c62f17af72bd01aee4397d629ee3dacbd4ea
Sehrish Kanwal <kanwals@unimelb.edu.au>
2019-01-29 15:33:00 +1100

    fixing tag name

--- terraform/stacks/mosdepth/main.tf
+++ terraform/stacks/mosdepth/main.tf
@@ -1,5 +1,5 @@
 # Create a new instance of the latest Ubuntu 14.04 on an
-# t2.micro node with an AWS Tag naming it "HelloWorld"
+# t2.micro node with an AWS Tag naming it "mosdepth-skanwal"
 provider "aws" {
   region = "ap-southeast-2"
   profile = "umccr_ops_admin_dev"

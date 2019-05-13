b5842d3257c7430d46870fddcb4e090c3756360a
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2019-05-13 14:29:58 +1000

    Remove AGHA resources, except buckets

--- terraform/stacks/agha_gdr/main.tf
+++ terraform/stacks/agha_gdr/main.tf
@@ -38,78 +38,78 @@ locals {
 # users, assume role policies, etc ...
 # => bastion account
 
-module "users" {
-  providers = {
-    aws.account = "aws.bastion"
-  }
+# module "users" {
+#   providers = {
+#     aws.account = "aws.bastion"
+#   }
 
-  source = "../../modules/iam_user/secure_users"
-  users  = "${var.agha_users_map}"
-}
+#   source = "../../modules/iam_user/secure_users"
+#   users  = "${var.agha_users_map}"
+# }
 
-resource "aws_iam_user_login_profile" "users_login" {
-  provider   = "aws.bastion"
-  count      = "${length(keys(var.agha_users_map))}"
-  user       = "${element(keys(var.agha_users_map), count.index)}"
-  pgp_key    = "${element(values(var.agha_users_map), count.index)}"
-  depends_on = ["module.users"]
-}
+# resource "aws_iam_user_login_profile" "users_login" {
+#   provider   = "aws.bastion"
+#   count      = "${length(keys(var.agha_users_map))}"
+#   user       = "${element(keys(var.agha_users_map), count.index)}"
+#   pgp_key    = "${element(values(var.agha_users_map), count.index)}"
+#   depends_on = ["module.users"]
+# }
 
-resource "aws_iam_group" "group" {
-  provider   = "aws.bastion"
-  count      = "${length(keys(var.group_members_map))}"
-  name       = "${element(keys(var.group_members_map), count.index)}"
-  depends_on = ["module.users"]
-}
+# resource "aws_iam_group" "group" {
+#   provider   = "aws.bastion"
+#   count      = "${length(keys(var.group_members_map))}"
+#   name       = "${element(keys(var.group_members_map), count.index)}"
+#   depends_on = ["module.users"]
+# }
 
-resource "aws_iam_group_membership" "group_members" {
-  provider   = "aws.bastion"
-  count      = "${length(keys(var.group_members_map))}"
-  name       = "${element(keys(var.group_members_map), count.index)}_membership"
-  users      = "${var.group_members_map[element(keys(var.group_members_map), count.index)]}"
-  group      = "${element(keys(var.group_members_map), count.index)}"
-  depends_on = ["aws_iam_group.group"]
-}
+# resource "aws_iam_group_membership" "group_members" {
+#   provider   = "aws.bastion"
+#   count      = "${length(keys(var.group_members_map))}"
+#   name       = "${element(keys(var.group_members_map), count.index)}_membership"
+#   users      = "${var.group_members_map[element(keys(var.group_members_map), count.index)]}"
+#   group      = "${element(keys(var.group_members_map), count.index)}"
+#   depends_on = ["aws_iam_group.group"]
+# }
 
-resource "aws_iam_group_policy" "group_assume_policy" {
-  provider   = "aws.bastion"
-  count      = "${length(keys(var.group_roles_map))}"
-  group      = "${element(keys(var.group_roles_map), count.index)}"
-  depends_on = ["aws_iam_group.group"]
+# resource "aws_iam_group_policy" "group_assume_policy" {
+#   provider   = "aws.bastion"
+#   count      = "${length(keys(var.group_roles_map))}"
+#   group      = "${element(keys(var.group_roles_map), count.index)}"
+#   depends_on = ["aws_iam_group.group"]
 
-  # define which roles the group members can assume
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Effect": "Allow",
-      "Action": [ "sts:AssumeRole" ],
-      "Resource": ${jsonencode(formatlist("arn:aws:iam::%s:role/%s", data.aws_caller_identity.current.account_id, var.group_roles_map[element(keys(var.group_roles_map), count.index)]))}
-    }
-  ]
-}
-EOF
-}
+#   # define which roles the group members can assume
+#   policy = <<EOF
+# {
+#   "Version": "2012-10-17",
+#   "Statement": [
+#     {
+#       "Effect": "Allow",
+#       "Action": [ "sts:AssumeRole" ],
+#       "Resource": ${jsonencode(formatlist("arn:aws:iam::%s:role/%s", data.aws_caller_identity.current.account_id, var.group_roles_map[element(keys(var.group_roles_map), count.index)]))}
+#     }
+#   ]
+# }
+# EOF
+# }
 
 ########## Dedicated user to generate long lived presigned URLs
 # See: https://aws.amazon.com/premiumsupport/knowledge-center/presigned-url-s3-bucket-expiration/
 
-module "agha_bot_user" {
-  source   = "../../modules/iam_user/secure_user"
-  username = "agha_bot"
-  pgp_key  = "keybase:freisinger"
-}
+# module "agha_bot_user" {
+#   source   = "../../modules/iam_user/secure_user"
+#   username = "agha_bot"
+#   pgp_key  = "keybase:freisinger"
+# }
 
-resource "aws_iam_user_policy_attachment" "ahga_bot_staging_rw" {
-  user       = "${module.agha_bot_user.username}"
-  policy_arn = "${aws_iam_policy.agha_staging_rw_policy.arn}"
-}
+# resource "aws_iam_user_policy_attachment" "ahga_bot_staging_rw" {
+#   user       = "${module.agha_bot_user.username}"
+#   policy_arn = "${aws_iam_policy.agha_staging_rw_policy.arn}"
+# }
 
-resource "aws_iam_user_policy_attachment" "ahga_bot_store_ro" {
-  user       = "${module.agha_bot_user.username}"
-  policy_arn = "${aws_iam_policy.agha_store_ro_policy.arn}"
-}
+# resource "aws_iam_user_policy_attachment" "ahga_bot_store_ro" {
+#   user       = "${module.agha_bot_user.username}"
+#   policy_arn = "${aws_iam_policy.agha_store_ro_policy.arn}"
+# }
 
 ################################################################################
 # bucket(s), roles, policies, role - policy attachments, etc ...
@@ -175,70 +175,70 @@ resource "aws_s3_bucket" "agha_gdr_log" {
   }
 }
 
-resource "aws_iam_role" "agha_member_roles" {
-  count                = "${length(keys(var.group_members_map))}"
-  name                 = "${element(keys(var.group_members_map), count.index)}"
-  path                 = "/"
-  assume_role_policy   = "${file("policies/assume-role-from-bastion.json")}"
-  max_session_duration = "43200"
-}
+# resource "aws_iam_role" "agha_member_roles" {
+#   count                = "${length(keys(var.group_members_map))}"
+#   name                 = "${element(keys(var.group_members_map), count.index)}"
+#   path                 = "/"
+#   assume_role_policy   = "${file("policies/assume-role-from-bastion.json")}"
+#   max_session_duration = "43200"
+# }
 
-data "template_file" "agha_staging_rw_policy" {
-  template = "${file("policies/bucket-rw-policy.json")}"
+# data "template_file" "agha_staging_rw_policy" {
+#   template = "${file("policies/bucket-rw-policy.json")}"
 
-  vars {
-    bucket_name = "${aws_s3_bucket.agha_gdr_staging.id}"
-  }
-}
+#   vars {
+#     bucket_name = "${aws_s3_bucket.agha_gdr_staging.id}"
+#   }
+# }
 
-data "template_file" "agha_store_ro_policy" {
-  template = "${file("policies/bucket-ro-policy.json")}"
+# data "template_file" "agha_store_ro_policy" {
+#   template = "${file("policies/bucket-ro-policy.json")}"
 
-  vars {
-    bucket_name = "${aws_s3_bucket.agha_gdr_store.id}"
-  }
-}
+#   vars {
+#     bucket_name = "${aws_s3_bucket.agha_gdr_store.id}"
+#   }
+# }
 
-data "template_file" "agha_store_rw_policy" {
-  template = "${file("policies/bucket-rw-policy.json")}"
+# data "template_file" "agha_store_rw_policy" {
+#   template = "${file("policies/bucket-rw-policy.json")}"
 
-  vars {
-    bucket_name = "${aws_s3_bucket.agha_gdr_store.id}"
-  }
-}
+#   vars {
+#     bucket_name = "${aws_s3_bucket.agha_gdr_store.id}"
+#   }
+# }
 
-resource "aws_iam_policy" "agha_staging_rw_policy" {
-  name   = "agha_staging_rw_policy"
-  path   = "/"
-  policy = "${data.template_file.agha_staging_rw_policy.rendered}"
-}
+# resource "aws_iam_policy" "agha_staging_rw_policy" {
+#   name   = "agha_staging_rw_policy"
+#   path   = "/"
+#   policy = "${data.template_file.agha_staging_rw_policy.rendered}"
+# }
 
-resource "aws_iam_policy" "agha_store_ro_policy" {
-  name   = "agha_store_ro_policy"
-  path   = "/"
-  policy = "${data.template_file.agha_store_ro_policy.rendered}"
-}
+# resource "aws_iam_policy" "agha_store_ro_policy" {
+#   name   = "agha_store_ro_policy"
+#   path   = "/"
+#   policy = "${data.template_file.agha_store_ro_policy.rendered}"
+# }
 
-resource "aws_iam_policy" "agha_store_rw_policy" {
-  name   = "agha_store_rw_policy"
-  path   = "/"
-  policy = "${data.template_file.agha_store_rw_policy.rendered}"
-}
+# resource "aws_iam_policy" "agha_store_rw_policy" {
+#   name   = "agha_store_rw_policy"
+#   path   = "/"
+#   policy = "${data.template_file.agha_store_rw_policy.rendered}"
+# }
 
-resource "aws_iam_role_policy_attachment" "agha_staging_rw_policy_attachment" {
-  count      = "${length(var.agha_staging_rw)}"
-  role       = "${var.agha_staging_rw[count.index]}"
-  policy_arn = "${aws_iam_policy.agha_staging_rw_policy.arn}"
-}
+# resource "aws_iam_role_policy_attachment" "agha_staging_rw_policy_attachment" {
+#   count      = "${length(var.agha_staging_rw)}"
+#   role       = "${var.agha_staging_rw[count.index]}"
+#   policy_arn = "${aws_iam_policy.agha_staging_rw_policy.arn}"
+# }
 
-resource "aws_iam_role_policy_attachment" "agha_store_ro_policy_attachment" {
-  count      = "${length(var.agha_store_ro)}"
-  role       = "${var.agha_store_ro[count.index]}"
-  policy_arn = "${aws_iam_policy.agha_store_ro_policy.arn}"
-}
+# resource "aws_iam_role_policy_attachment" "agha_store_ro_policy_attachment" {
+#   count      = "${length(var.agha_store_ro)}"
+#   role       = "${var.agha_store_ro[count.index]}"
+#   policy_arn = "${aws_iam_policy.agha_store_ro_policy.arn}"
+# }
 
-resource "aws_iam_role_policy_attachment" "agha_store_rw_policy_attachment" {
-  count      = "${length(var.agha_store_rw)}"
-  role       = "${var.agha_store_rw[count.index]}"
-  policy_arn = "${aws_iam_policy.agha_store_rw_policy.arn}"
-}
+# resource "aws_iam_role_policy_attachment" "agha_store_rw_policy_attachment" {
+#   count      = "${length(var.agha_store_rw)}"
+#   role       = "${var.agha_store_rw[count.index]}"
+#   policy_arn = "${aws_iam_policy.agha_store_rw_policy.arn}"
+# }
--- terraform/stacks/agha_gdr/outputs.tf
+++ terraform/stacks/agha_gdr/outputs.tf
@@ -1,25 +1,25 @@
-output "user_key_ids" {
-  value = "${module.users.access_keys}"
-}
+# output "user_key_ids" {
+#   value = "${module.users.access_keys}"
+# }
 
-output "user_secure_keys" {
-  value = "${module.users.encrypted_secret_access_keys}"
-}
+# output "user_secure_keys" {
+#   value = "${module.users.encrypted_secret_access_keys}"
+# }
 
-output "user_logins" {
-  value = "${zipmap(aws_iam_user_login_profile.users_login.*.user, aws_iam_user_login_profile.users_login.*.encrypted_password)}"
-}
+# output "user_logins" {
+#   value = "${zipmap(aws_iam_user_login_profile.users_login.*.user, aws_iam_user_login_profile.users_login.*.encrypted_password)}"
+# }
 
 
 # output for serivce user: agha_bot_user
-output "agha_bot_user_username" {
-  value = "${module.agha_bot_user.username}"
-}
+# output "agha_bot_user_username" {
+#   value = "${module.agha_bot_user.username}"
+# }
 
-output "agha_bot_user_access_key" {
-  value = "${module.agha_bot_user.access_key}"
-}
+# output "agha_bot_user_access_key" {
+#   value = "${module.agha_bot_user.access_key}"
+# }
 
-output "uagha_bot_user_secret_access_key" {
-  value = "${module.agha_bot_user.encrypted_secret_access_key}"
-}
+# output "uagha_bot_user_secret_access_key" {
+#   value = "${module.agha_bot_user.encrypted_secret_access_key}"
+# }

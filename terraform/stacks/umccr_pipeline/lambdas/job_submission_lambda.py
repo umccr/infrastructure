7448c8bff80d67c408bab6264d25e7d8f22ea34d
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2019-06-05 16:42:21 +1000

    Update pipeline to sync to Spartan S3 instead of FS

--- terraform/stacks/umccr_pipeline/lambdas/job_submission_lambda.py
+++ terraform/stacks/umccr_pipeline/lambdas/job_submission_lambda.py
@@ -43,6 +43,7 @@ stats_update_script = getSSMParam(SSM_PARAM_PREFIX + "stats_update_script")
 hpc_sync_dest_host = getSSMParam(SSM_PARAM_PREFIX + "hpc_sync_dest_host")
 HPC_SSH_USER = getSSMParam(SSM_PARAM_PREFIX + "hpc_sync_ssh_user")
 aws_profile = getSSMParam(SSM_PARAM_PREFIX + "aws_profile")
+aws_profile_spartan = getSSMParam(SSM_PARAM_PREFIX + "aws_profile_spartan")
 s3_sync_dest_bucket = getSSMParam(SSM_PARAM_PREFIX + "s3_sync_dest_bucket")
 
 
@@ -85,17 +86,16 @@ def build_command(script_case, input_data):
         execution_timneout = '36000'
         command += f" DEPLOY_ENV={DEPLOY_ENV} AWS_PROFILE={aws_profile}"
         command += f" {checksum_script} bcl2fastq {bcl2fastq_out_path} {runfolder}"
-    elif script_case == "sync_runfolder_to_hpc":
+    elif script_case == "sync_runfolder_to_s3_spartan":
         execution_timneout = '10800'
-        command += f" DEPLOY_ENV={DEPLOY_ENV} AWS_PROFILE={aws_profile}"
-        command += f" {hpc_sync_script} -d {hpc_sync_dest_host} -u {HPC_SSH_USER} -p {hpc_dest_base_path}"
-        command += f" -s {runfolder_path} -x Data -x Thumbnail_Images -n {runfolder}"
-    elif script_case == "sync_fastqs_to_hpc":
+        command += f" DEPLOY_ENV={DEPLOY_ENV} AWS_PROFILE={aws_profile_spartan}"
+        command += f" {s3_sync_script} -b {s3_sync_dest_bucket} -n {runfolder}"
+        command += f" -d {runfolder} -s {runfolder_path} -x Data/* -x Thumbnail_Images/*"
+    elif script_case == "sync_fastqs_to_s3_spartan":
         execution_timneout = '36000'
-        dest_path = os.path.join(hpc_dest_base_path, runfolder)
-        command += f" DEPLOY_ENV={DEPLOY_ENV} AWS_PROFILE={aws_profile}"
-        command += f" {hpc_sync_script} -d {hpc_sync_dest_host} -u {HPC_SSH_USER} -p {dest_path}"
-        command += f" -s {bcl2fastq_out_path} -n {runfolder}"
+        command += f" DEPLOY_ENV={DEPLOY_ENV} AWS_PROFILE={aws_profile_spartan}"
+        command += f" {s3_sync_script} -b {s3_sync_dest_bucket} -n {runfolder}"
+        command += f" -d {runfolder}/{runfolder} -s {bcl2fastq_out_path} -f"
     elif script_case == "sync_runfolder_to_s3":
         execution_timneout = '10800'
         command += f" DEPLOY_ENV={DEPLOY_ENV} AWS_PROFILE={aws_profile}"
@@ -168,40 +168,6 @@ def lambda_handler(event, context):
         print(f"Task token: {task_token[:10]}...{task_token[-10:]}")
     else:
         raise ValueError("Activity response did not yield a task token!")
-
-    ############################################################
-    # send a command to the SSM for asynchronous execution
-
-    script_command, script_timeout = build_command(script_case=script_execution,
-                                                   input_data=event['input'])
-
-    session_assumed = aws_session(role_arn=BASTION_SSM_ROLE_ARN, session_name='bastion_session')
-    response = session_assumed.client('ssm').send_command(
-        InstanceIds=[ssm_instance_id],
-        DocumentName=SSM_DOC_NAME,
-        Parameters={"commands": [script_command], "executionTimeout": [script_timeout],
-                    "taskToken": [task_token], "deployEnv": [DEPLOY_ENV]},
-        CloudWatchOutputConfig={'CloudWatchOutputEnabled': True}, )
-
-    command_id = response['Command']['CommandId']
-    print(f"Command ID: {command_id}")
-
-
-def manual_handling(event):
-    print("Received event: " + json.dumps(event, indent=2))
-
-    ############################################################
-    # initial checks
-
-    if event.get('script_execution'):
-        script_execution = event['script_execution']
-        print(f"script_execution: {script_execution}")
-    else:
-        raise ValueError('A script_execution parameter is mandatory!')
-
-    ############################################################
-    # Mock the task token from the wait-for activity task
-    task_token = '12345token54321'
 
     ############################################################
     # send a command to the SSM for asynchronous execution

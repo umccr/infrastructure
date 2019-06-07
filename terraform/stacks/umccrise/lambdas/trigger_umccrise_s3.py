8dd0cc3b1bd263ad96d23d4cceaa5ea83df51fff
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2019-06-07 15:29:36 +1000

    Update umccrise README. Fix path issue in S3 trigger lambda

--- terraform/stacks/umccrise/lambdas/trigger_umccrise_s3.py
+++ terraform/stacks/umccrise/lambdas/trigger_umccrise_s3.py
@@ -1,25 +1,25 @@
 import json
 import boto3
-import base64
 import os
+
 
 def lambda_handler(event, context):
     client = boto3.client('lambda')
 
-    #environment variables
+    # environment variables
     UMCCRISE_MEM = os.environ.get("UMCCRISE_MEM")
     UMCCRISE_VCPUS = os.environ.get("UMCCRISE_VCPUS")
     UMCCRISE_FUNCTION_NAME = os.environ.get("UMCCRISE_FUNCTION_NAME")
 
-    #event variables
+    # event variables
     bucket = event["Records"][0]["s3"]["bucket"]["name"]
     obj = event["Records"][0]["s3"]["object"]["key"]
-    data_dir = obj.split("/")[0]
-    job_name = bucket + "---" + data_dir 
-    
+    data_dir = os.path.dirname(obj)
+    job_name = bucket + "---" + data_dir.replace('/', '_')
+
     request_payload = {
-         "resultDir": data_dir, 
-         "memory": UMCCRISE_MEM, 
+         "resultDir": data_dir,
+         "memory": UMCCRISE_MEM,
          "vcpus": UMCCRISE_VCPUS,
          "jobName": job_name
     }
@@ -30,20 +30,21 @@ def lambda_handler(event, context):
     #     "env": {"ctx": "ctx"},
     #     "client": {}
     # }
-    
+
     print("Invoking lambda with payload: ", request_payload)
     response = client.invoke(
-    FunctionName=UMCCRISE_FUNCTION_NAME,
-    InvocationType='RequestResponse',
-    LogType='Tail',
-    Payload=json.dumps(request_payload).encode('utf-8'),
-    Qualifier='$LATEST'
-)
+        FunctionName=UMCCRISE_FUNCTION_NAME,
+        InvocationType='RequestResponse',
+        LogType='Tail',
+        Payload=json.dumps(request_payload).encode('utf-8'),
+        Qualifier='$LATEST'
+    )
 
     if response["StatusCode"] is 200:
-        message = "Detected {} was uploaded to {} and fired umccrise successfully.".format(obj,bucket)
+        message = f"Detected {obj} was uploaded to {bucket} and fired umccrise successfully."
     else:
-        message = "Detected {} was uploaded to {} and there was an error firing umccrise. Error code from lambda was {}".format(obj,bucket,response["StatusCode"])
+        message = f"Detected {obj} was uploaded to {bucket} and there was an error firing umccrise."
+        message += f"Error code from lambda was {response['StatusCode']}."
 
     print(message)
 
@@ -58,4 +59,3 @@ def lambda_handler(event, context):
     }
 
     return response
-

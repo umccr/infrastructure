458ba9b67ce2c0eb3d61a0bcf6e7c41b42c73bf7
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2019-09-17 17:41:14 +1000

    Update batch module to allow switch between spot and on-demand instances

--- terraform/modules/batch/main.tf
+++ terraform/modules/batch/main.tf
@@ -5,15 +5,18 @@
 resource "aws_iam_instance_profile" "ecsInstanceRole" {
   name = "${aws_iam_role.ecsInstanceRole.name}"
   role = "${aws_iam_role.ecsInstanceRole.name}"
+
   # NOTE: AWS batch does not support profiles with non-root path!
   path = "/"
+
   # make sure the policy is attached before creating the profile
-  depends_on   = ["aws_iam_role_policy_attachment.ecsInstanceRole"]
+  depends_on = ["aws_iam_role_policy_attachment.ecsInstanceRole"]
 }
 
 resource "aws_iam_role" "ecsInstanceRole" {
   name = "${var.stack_name}_ecsInstanceRole${var.name_suffix}"
   path = "/${var.stack_name}/"
+
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -31,17 +34,17 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "ecsInstanceRole" {
-  role = "${aws_iam_role.ecsInstanceRole.name}"
+  role       = "${aws_iam_role.ecsInstanceRole.name}"
   policy_arn = "${var.ec2_additional_policy}"
 }
 
 resource "aws_iam_role_policy_attachment" "ssmDefault" {
-  role = "${aws_iam_role.ecsInstanceRole.name}"
+  role       = "${aws_iam_role.ecsInstanceRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforSSM"
 }
 
 resource "aws_iam_role_policy_attachment" "ecsInstanceRoleDefault" {
-  role = "${aws_iam_role.ecsInstanceRole.name}"
+  role       = "${aws_iam_role.ecsInstanceRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
 }
 
@@ -51,6 +54,7 @@ resource "aws_iam_role_policy_attachment" "ecsInstanceRoleDefault" {
 resource "aws_iam_role" "AWSBatchServiceRole" {
   name = "${var.stack_name}_AWSBatchServiceRole${var.name_suffix}"
   path = "/${var.stack_name}/"
+
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -68,7 +72,7 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "AWSBatchServiceRole" {
-  role = "${aws_iam_role.AWSBatchServiceRole.name}"
+  role       = "${aws_iam_role.AWSBatchServiceRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSBatchServiceRole"
 }
 
@@ -76,9 +80,10 @@ resource "aws_iam_role_policy_attachment" "AWSBatchServiceRole" {
 # create SPOT fleet service role
 
 resource "aws_iam_role" "AmazonEC2SpotFleetTaggingRole" {
-  name = "${var.stack_name}_AmazonEC2SpotFleetTaggingRole${var.name_suffix}"
-  path = "/${var.stack_name}/"
+  name        = "${var.stack_name}_AmazonEC2SpotFleetTaggingRole${var.name_suffix}"
+  path        = "/${var.stack_name}/"
   description = "Role to Allow EC2 Spot Fleet to request and terminate Spot Instances on your behalf."
+
   assume_role_policy = <<POLICY
 {
   "Version": "2012-10-17",
@@ -97,44 +102,67 @@ POLICY
 }
 
 resource "aws_iam_role_policy_attachment" "AmazonEC2SpotFleetTaggingRole" {
-  role = "${aws_iam_role.AmazonEC2SpotFleetTaggingRole.name}"
+  role       = "${aws_iam_role.AmazonEC2SpotFleetTaggingRole.name}"
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2SpotFleetTaggingRole"
 }
 
 ################################################################################
 # create ECS compute environment
 
-resource "aws_batch_compute_environment" "batch" {
+resource "aws_batch_compute_environment" "batch_ondemand" {
+  count                    = "${var.use_spot == "true" ? 0 : 1}"
   compute_environment_name = "${var.compute_env_name}"
+  type                     = "MANAGED"
+  service_role             = "${aws_iam_role.AWSBatchServiceRole.arn}"
 
   compute_resources {
-    instance_role = "${aws_iam_instance_profile.ecsInstanceRole.arn}"
-    image_id      = "${var.image_id}"
-
-    instance_type = "${var.instance_types}"
-
-    max_vcpus     = "${var.max_vcpus}"
-    min_vcpus     = "${var.min_vcpus}"
-
+    type               = "EC2"
+    instance_role      = "${aws_iam_instance_profile.ecsInstanceRole.arn}"
+    image_id           = "${var.image_id}"
+    instance_type      = "${var.instance_types}"
+    max_vcpus          = "${var.max_vcpus}"
+    min_vcpus          = "${var.min_vcpus}"
     security_group_ids = ["${var.security_group_ids}"]
-
-    subnets = ["${var.subnet_ids}"]
+    subnets            = ["${var.subnet_ids}"]
 
     tags = {
       Name  = "batch"
       stack = "${var.stack_name}"
     }
-
-    spot_iam_fleet_role = "${aws_iam_role.AmazonEC2SpotFleetTaggingRole.arn}"
-    type                = "SPOT"
-    bid_percentage      = "${var.spot_bid_percent}"
   }
 
-  service_role = "${aws_iam_role.AWSBatchServiceRole.arn}"
-  type         = "MANAGED"
-  depends_on   = ["aws_iam_role_policy_attachment.AWSBatchServiceRole", 
-                  "aws_iam_role_policy_attachment.AmazonEC2SpotFleetTaggingRole",
-                  "aws_iam_instance_profile.ecsInstanceRole"
-                  ]
+  depends_on = ["aws_iam_role_policy_attachment.AWSBatchServiceRole",
+    "aws_iam_role_policy_attachment.AmazonEC2SpotFleetTaggingRole",
+    "aws_iam_instance_profile.ecsInstanceRole",
+  ]
 }
 
+resource "aws_batch_compute_environment" "batch" {
+  count                    = "${var.use_spot == "true" ? 1 : 0}"
+  compute_environment_name = "${var.compute_env_name}"
+  type                     = "MANAGED"
+  service_role             = "${aws_iam_role.AWSBatchServiceRole.arn}"
+
+  compute_resources {
+    type                = "SPOT"
+    spot_iam_fleet_role = "${aws_iam_role.AmazonEC2SpotFleetTaggingRole.arn}"
+    bid_percentage      = "${var.spot_bid_percent}"
+    instance_role       = "${aws_iam_instance_profile.ecsInstanceRole.arn}"
+    image_id            = "${var.image_id}"
+    instance_type       = "${var.instance_types}"
+    max_vcpus           = "${var.max_vcpus}"
+    min_vcpus           = "${var.min_vcpus}"
+    security_group_ids  = ["${var.security_group_ids}"]
+    subnets             = ["${var.subnet_ids}"]
+
+    tags = {
+      Name  = "batch"
+      stack = "${var.stack_name}"
+    }
+  }
+
+  depends_on = ["aws_iam_role_policy_attachment.AWSBatchServiceRole",
+    "aws_iam_role_policy_attachment.AmazonEC2SpotFleetTaggingRole",
+    "aws_iam_instance_profile.ecsInstanceRole",
+  ]
+}
--- terraform/modules/batch/outputs.tf
+++ terraform/modules/batch/outputs.tf
@@ -1,5 +1,7 @@
 output "compute_env_arn" {
-  value = "${aws_batch_compute_environment.batch.arn}"
+  # we need to use the splat syntax, since 
+  # value = "${aws_batch_compute_environment.batch.0.arn}"
+  value = "${concat(aws_batch_compute_environment.batch.*.arn, aws_batch_compute_environment.batch_ondemand.*.arn)}"
 }
 
 /*output "underlying_ecs_cluster" {

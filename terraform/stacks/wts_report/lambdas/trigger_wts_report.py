8320beab18a41cfa906c6562e337e20352a6a83f
Sehrish Kanwal <kanwals@unimelb.edu.au>
2019-10-04 11:04:46 +1000

    add a check on the user defined input paths in the lamda invoke command

--- terraform/stacks/wts_report/lambdas/trigger_wts_report.py
+++ terraform/stacks/wts_report/lambdas/trigger_wts_report.py
@@ -26,37 +26,59 @@ def lambda_handler(event, context):
     job_name = data_bucket + "---" + data_wts_dir.replace('/', '_').replace('.', '_')
     job_name = os.environ.get('JOBNAME_PREFIX') + '_' + job_name
 
+    try:
+        response_wts = s3.list_objects(Bucket=data_bucket, MaxKeys=5, Prefix=data_wts_dir)
+        print("S3 list response: " + json.dumps(response_wts, indent=2, sort_keys=True, default=str))
+        if not response_wts.get('Contents') or len(response_wts['Contents']) < 1:
+            return {
+                'statusCode': 400,
+                'error': 'Bad parameter',
+                'message': f"Provided S3 path ({data_wts_dir}) does not exist in bucket {data_bucket}!"
+            }
+
+        response_wgs = s3.list_objects(Bucket=data_bucket, MaxKeys=5, Prefix=data_wgs_dir)
+        print("S3 list response: " + json.dumps(response_wgs, indent=2, sort_keys=True, default=str))
+        if not response_wgs.get('Contents') or len(response_wgs['Contents']) < 1:
+            return {
+                'statusCode': 400,
+                'error': 'Bad parameter',
+                'message': f"Provided S3 path ({data_wgs_dir}) does not exist in bucket {data_bucket}!"
+            }
+
     # create and submit a Batch job request
-    container_overrides['environment'] = [
-        {'name': 'S3_WTS_INPUT_DIR', 'value': data_wts_dir},
-        {'name': 'S3_WGS_INPUT_DIR', 'value': data_wgs_dir},
-        {'name': 'S3_DATA_BUCKET', 'value': data_bucket},
-        {'name': 'S3_REFDATA_BUCKET', 'value': refdata_bucket},
-        {'name': 'REF_DATASET', 'value': ref_dataset}
-    ]
+        container_overrides['environment'] = [
+            {'name': 'S3_WTS_INPUT_DIR', 'value': data_wts_dir},
+            {'name': 'S3_WGS_INPUT_DIR', 'value': data_wgs_dir},
+            {'name': 'S3_DATA_BUCKET', 'value': data_bucket},
+            {'name': 'S3_REFDATA_BUCKET', 'value': refdata_bucket},
+            {'name': 'REF_DATASET', 'value': ref_dataset}
+        ]
 
-    if container_mem:
-        container_overrides['memory'] = int(container_mem)
-    if container_vcpus:
-        container_overrides['vcpus'] = int(container_vcpus)
-        parameters['vcpus'] = container_vcpus
+        if container_mem:
+            container_overrides['memory'] = int(container_mem)
+        if container_vcpus:
+            container_overrides['vcpus'] = int(container_vcpus)
+            parameters['vcpus'] = container_vcpus
 
-    print("jobName: " + job_name)
-    print("containerOverrides: ")
-    print(container_overrides)
-    print("jobDefinition: ")
-    print(job_definition)
-    response = batch_client.submit_job(
-        dependsOn=depends_on,
-        containerOverrides=container_overrides,
-        jobDefinition=job_definition,
-        jobName=job_name,
-        jobQueue=job_queue,
-        parameters=parameters
-    )
+        print("jobName: " + job_name)
+        print("containerOverrides: ")
+        print(container_overrides)
+        print("jobDefinition: ")
+        print(job_definition)
+        response = batch_client.submit_job(
+            dependsOn=depends_on,
+            containerOverrides=container_overrides,
+            jobDefinition=job_definition,
+            jobName=job_name,
+            jobQueue=job_queue,
+            parameters=parameters
+        )
 
-    # Log response from AWS Batch
-    print("Response: " + json.dumps(response, indent=2))
-    # Return the jobId
-    event['jobId'] = response['jobId']
-    return event
+        # Log response from AWS Batch
+        print("Response: " + json.dumps(response, indent=2))
+        # Return the jobId
+        event['jobId'] = response['jobId']
+        return event
+
+    except Exception as e:
+        print(e)

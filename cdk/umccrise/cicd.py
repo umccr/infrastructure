7eceac53ab5ff702a81b488bd0be35317de86fa2
Roman Valls Guimera <brainstorm@nopcode.org>
2019-10-22 14:42:07 +1100

    Tweak/Migrate codebuild CDK for new dev, add related policies needed for ECR and dind, MEDIUM compute env setup

--- cdk/umccrise/cicd.py
+++ cdk/umccrise/cicd.py
@@ -2,29 +2,58 @@ from aws_cdk import (
     core,
     aws_codebuild as cb,
     aws_codecommit as cc,
-    aws_codepipeline as cp
+    aws_codepipeline as cp,
+    aws_iam as iam,
 )
 
 class CICDStack(core.Stack):
     def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
-        cb.Project(self, id = "umccrise",
-                    environment = { "buildImage": cb.LinuxBuildImage.STANDARD_2_0, "privileged": True},
+        build_env = cb.BuildEnvironment(build_image=cb.LinuxBuildImage.from_docker_registry("docker:dind"),
+                                        privileged=True,
+                                        compute_type=cb.ComputeType.MEDIUM);
+
+        cb_project = cb.Project(self, id = "umccrise",
+                    environment = build_env,
                     source = cb.Source.git_hub(
                         identifier = "umccrise",
                         owner = "umccr",
                         repo = "umccrise",
                         clone_depth = 1,
-                        webhook = True,
-                    )
-                  )
+                        webhook = True
+                    ),
+                )
+
+        # https://stackoverflow.com/questions/38587325/aws-ecr-getauthorizationtoken/54806087
+        cb_project.role.add_managed_policy(iam.ManagedPolicy.from_aws_managed_policy_name('AmazonEC2ContainerRegistryPowerUser'))
 
 app = core.App()
 CICDStack(app, "UmccriseCICDStack")
 
-# XXX: https://stackoverflow.com/questions/38587325/aws-ecr-getauthorizationtoken/54806087
-# Because of GitHub MFA, we are getting "is not authorized to perform: ecr:GetAuthorizationToken on resource: *"
-# Assigning Policy to CICD role "AmazonEC2ContainerRegistryPowerUser" for now...
-
 app.synth()
+
+### XXX: 
+# {
+#     "Version": "2012-10-17",
+#     "Statement": [
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "s3:ListBucket"
+#             ],
+#             "Resource": [
+#                 "arn:aws:s3:::umccr-refdata-dev"
+#             ]
+#         },
+#         {
+#             "Effect": "Allow",
+#             "Action": [
+#                 "s3:GetObject"
+#             ],
+#             "Resource": [
+#                 "arn:aws:s3:::umccr-refdata-dev/*"
+#             ]
+#         }
+#     ]
+# }

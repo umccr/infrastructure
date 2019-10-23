ee726fab2e59de3e897196804d3a134186f70b7b
Roman Valls Guimera <brainstorm@nopcode.org>
2019-10-23 11:36:25 +1100

    Back to working status, pinning to new dev account

--- cdk/apps/umccrise/app.py
+++ cdk/apps/umccrise/app.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 from aws_cdk import core
-from umccrise.cicd import CICDStack
+from stacks.cicd import CICDStack
 
 app = core.App()
-CICDStack(app, "umccrise-cicd", env = {'region': 'ap-southeast-2'})
+CICDStack(app, "umccrise-cicd", env = {'account': '843407916570', 'region': 'ap-southeast-2'})
 
 app.synth()

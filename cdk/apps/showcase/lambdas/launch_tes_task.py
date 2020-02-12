cf09fd520d43d1210b451ecf1f9de9efe70f6131
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2020-02-12 17:33:52 +1100

    Add support for optional map parameter

--- cdk/apps/showcase/lambdas/launch_tes_task.py
+++ cdk/apps/showcase/lambdas/launch_tes_task.py
@@ -81,6 +81,9 @@ def lambda_handler(event, context):
         }
     }
 
+    if event.get('item'):
+        body['arguments']['sample_list_name'] = event.get('item')
+
     # call the remote API
     status, data = send_request(
         method='POST',

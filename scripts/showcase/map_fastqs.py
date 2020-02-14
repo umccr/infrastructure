5c68e2da222a39b574cd7ee7a9182e75c497b06e
Roman Valls Guimera <brainstorm@nopcode.org>
2020-02-14 12:54:56 +1100

    add argparse to map_fastqs.py, make both samplesheet scripts runnable

--- scripts/showcase/map_fastqs.py
+++ scripts/showcase/map_fastqs.py
@@ -1,10 +1,24 @@
+#!/usr/bin/env python
+
+import os
+import argparse
 import pandas as pd
 
-df = pd.read_csv("fastqs.csv")
+#read inputs
+parser = argparse.ArgumentParser()
+parser.add_argument("--samplesheet", "-s", type=str, required=True)
+parser.add_argument("--outputDir", "-o", type=str, required=True)
+
+args = parser.parse_args()
+
+# create output directory if it does not exist
+os.makedirs(args.outputDir, exist_ok=True)
+
+df = pd.read_csv(args.samplesheet)
 
 df.sort_values(by="RGSM")
 df.set_index(keys="RGSM", drop=False, inplace=True)
 rgsms = df["RGSM"].unique().tolist()
 
 for rgsm in rgsms:
-    df.loc[df.RGSM==rgsm].to_csv(rgsm+".csv")
+    df.loc[df.RGSM==rgsm].to_csv(os.path.join(args.outputDir, rgsm+".csv"))

dd8b5a34e9f7013f2568d8790570dbf89991b564
Alexis Lucattini <alexis.lucattini@umccr.org>
2020-03-02 14:01:52 +1100

    Added tag set as recommended in wiki

--- cdk/apps/gridss-purple-lynx/app.py
+++ cdk/apps/gridss-purple-lynx/app.py
@@ -7,8 +7,24 @@ from gridss_purple_lynx.gridss_purple_lynx_stack import GridssPurpleLynxStack
 # Set account vars
 ACCOUNT = "843407916570"  # Dev
 REGION = "ap-southeast-2"
+CREATOR_NAME = "Alexis Lucattini"
 
+TAGS = {
+    # Name is set to gridss-purple-lynx
+    "Stack": "CDK",
+    "UseCase": "Testing-CDK",
+    "Creator": "Alexis Lucattini",
+    "Environment": "dev"
+}
+
+# Initialise app
 app = core.App()
+
+# Build stack on app
 GridssPurpleLynxStack(app, "gridss-purple-lynx", env={"account": ACCOUNT, "region": REGION})
+
+# Add tags to app
+for key, value in TAGS.items():
+    core.Tag.add(app, key, value)
 
 app.synth()

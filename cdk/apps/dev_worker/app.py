29e10dd581dcbf82ee53c98c990e5015a9a1d81f
Alexis Lucattini <alexis.lucattini@umccr.org>
2020-03-03 14:41:27 +1100

    Updated readme, greater flexibility through context,
    changed namings from gridss, write repo to environment

--- cdk/apps/dev_worker/app.py
+++ cdk/apps/dev_worker/app.py
@@ -4,8 +4,26 @@ from aws_cdk import core
 
 from dev_worker.dev_worker_stack import DevWorkerStack
 
+# Set account vars
+ACCOUNT = "843407916570"  # Dev
+REGION = "ap-southeast-2"
+CREATOR_NAME = "Alexis Lucattini"
+
+# Set tags for container
+TAGS = {
+    # Name is set to gridss-purple-lynx
+    "Stack": "DEV-CDK",
+    "UseCase": "Testing-CDK",
+    "Creator": "Alexis Lucattini",
+    "Environment": "dev"
+}
+
 
 app = core.App()
-DevWorkerStack(app, "dev-worker")
+DevWorkerStack(app, "dev-worker", env={"account": ACCOUNT, "region": REGION})
+
+# Add tags to app
+for key, value in TAGS.items():
+    core.Tag.add(app, key, value)
 
 app.synth()

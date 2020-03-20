247d58d9a4f4c2cca0049f4b3ec5bbb8569d2342
alexiswl <alexis.lucattini@umccr.org>
2020-03-20 18:38:33 +1100

    Added Spot Pricing to dev worker instance (#32)
    * Added dev_worker cdk app to git.
    
    Like the gridss app but without the docker pull and s3 pull commands.
    
    * Updated readme, greater flexibility through context, 
    
    changed namings from gridss, write repo to environment
    
    * Added a create env command in readme, added dependencies in setup.py
    
    * Added IAP to dev worker instance. Updated setup.py requirements to latest version of aws-cdk
    
    * Added SpotPricing to the dev worker instance
    
    * Added the folder dev/ to the list of files to ignore
    
    * Added a separate block device for the /var mount.
    Updated user_data to remount /var at the block device.
    Added LaunchTemplate property to the host instance.
    Renamed VOLUME_SIZE and deprecated mount point option.
    /mnt/xvdh now mounts to /data
    
    * Fixed mode settings for /data and fixed usermod commands.

--- cdk/apps/dev_worker/user_data/user_data.sh
+++ cdk/apps/dev_worker/user_data/user_data.sh
@@ -5,32 +5,55 @@
 ACCOUNT_ID=${__ACCOUNT_ID__}
 REGION=${__REGION__}
 
+# Other global vars
+IAP_DOWNLOAD_LINK="https://stratus-documentation-us-east-1-public.s3.amazonaws.com/cli/latest/linux/iap"
+
 # ECR Container vars
-EC2_REPO="${!ACCOUNT_ID}.dkr.ecr.${!REGION}.amazonaws.com/"
+EC2_REPO="${!ACCOUNT_ID}.dkr.ecr.${!REGION}.amazonaws.com"
 # Write this to /etc/environment for all users
-echo "export EC2_REPO=${!EC2_REPO}" >> "/etc/environment"
+echo "export EC2_REPO=${!EC2_REPO}" >> "/etc/profile.d/ec2_repo.sh"
 
 ## Fix time
 # Set time/logs to melbourne time
 ln -sf /usr/share/zoneinfo/Australia/Melbourne /etc/localtime
 
-## Fix mountpoints
-# create mount point directory
-mkdir /mnt/xvdh
-# create ext4 filesystem on new volume
-mkfs -t ext4 /dev/xvdh
-# add an entry to fstab to mount volume during boot
-echo "/dev/xvdh       /mnt/xvdh   ext4    rw,suid,dev,exec,auto,user,async,nofail 0       2" >> /etc/fstab
+# Put /var on its own partition
+mkfs -t ext4 /dev/sdf
+# Mount it temporarily under /mnt/var
+mkdir /mnt/var
+mount /dev/sdf /mnt/var
+# Go to single-user mode so we have no read/write activity
+# Move /var over to tmp mount
+# Unmount disk and then remount as /var
+rsync --archive --delete /var/ /mnt/var/
+umount /dev/sdf
+mount /dev/sdf /var
+# Delete the invalid mount (/mnt/var)
+rm -r /mnt/var
+
+# Add disk to fstab
+echo "/dev/sdf       /var   ext4    rw,suid,dev,exec,auto,user,async,nofail 0       2" >> /etc/fstab
 # mount the volume on current boot
 mount -a
 
-# create a user folder for both users on this directory
-mkdir /mnt/xvdh/ssm-user
-mkdir /mnt/xvdh/ec2-user
+## Now get the /data/ mountpoint
+# Create the communal group InstanceUser
+groupadd InstanceUser
+# create mount point didrectory for volume
+mkdir /data
+# create ext4 filesystem on extended volume for data
+mkfs -t ext4 /dev/sdg
+# add an entry to fstab to mount volume during boot
+echo "/dev/sdg       /data   ext4    rw,suid,dev,exec,auto,user,async,nofail 0       2" >> /etc/fstab
+# mount the volume on current boot
+mount -a
+# Make the /data mount a communal playground for all users
+chown root:InstanceUser /data
+chmod 775 /data
 
-# change the owner so the user (via SSM) has access
-chown -R ssm-user /mnt/xvdh/ssm-user
-chown -R ec2-user /mnt/xvdh/ec2-user
+# Add each user to the instance user group so they have access to the /data mount
+usermod -a -G InstanceUser ec2-user
+usermod -a -G InstanceUser ssm-user
 
 ## Update yum
 yum update -y -q
@@ -40,12 +63,6 @@ yum groupinstall -y 'Development Tools'
 
 # Install docker
 amazon-linux-extras install docker
-
-# Move docker folder to larger diskspace
-mv /var/lib/docker /mnt/xvdh/docker
-
-# Link mount to docker container
-ln -s /mnt/xvdh/docker /var/lib/docker
 
 # Add users to docker
 usermod -a -G docker ssm-user
@@ -63,7 +80,31 @@ su - "ssm-user" -c 'mkdir -p $HOME/.docker && echo "{ \"credsStore\" : \"ecr-log
 
 # Download IAP and install into /usr/local/bin
 echo "Downloading IAP"
-IAP_LINK="https://stratus-documentation-us-east-1-public.s3.amazonaws.com/cli/latest/linux/iap"
 (cd /usr/local/bin && \
-  wget "${IAP_LINK}" && \
+  wget "${!IAP_DOWNLOAD_LINK}" && \
   chmod +x iap)
+
+# Installing anaconda
+echo "Installing anaconda"
+ANACONDA_VERSION=2020.02
+mkdir -p /opt
+wget --quiet https://repo.anaconda.com/archive/Anaconda3-${!ANACONDA_VERSION}-Linux-x86_64.sh
+bash Anaconda3-${!ANACONDA_VERSION}-Linux-x86_64.sh -b -p /opt/conda
+rm Anaconda3-${!ANACONDA_VERSION}-Linux-x86_64.sh
+
+# Update conda and clean up
+/opt/conda/bin/conda update --yes \
+  --name base \
+  --channel defaults \
+	conda
+conda clean --all --yes
+
+# Install jupyter so one can launch notebook
+conda install --yes --freeze-installed \
+  --channel anaconda \
+  jupyter
+conda clean --all --yes
+
+# Fix bashrc for ec2-user and ssm-user for access ready for conda
+su - "ec2-user" -c "/opt/conda/bin/conda init"
+su - "ssm-user" -c "/opt/conda/bin/conda init"

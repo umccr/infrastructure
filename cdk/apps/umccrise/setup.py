aca31d214edde423dd05b98a12f61d3b451e6e98
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2020-09-08 12:19:06 +1000

    CDK umccrise - Update dependencies

--- cdk/apps/umccrise/setup.py
+++ cdk/apps/umccrise/setup.py
@@ -26,6 +26,7 @@ setuptools.setup(
         "aws_cdk.aws_ec2",
         "aws_cdk.aws_ecs",
         "aws_cdk.aws_ecr",
+        "aws_cdk.aws_events_targets",
         "aws_cdk.aws_iam",
         "aws_cdk.aws_lambda",
         "aws_cdk.aws_s3",

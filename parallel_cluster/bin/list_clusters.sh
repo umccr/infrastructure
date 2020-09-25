783e38fb10d4d4162e1957197ca1ba5d1873698d
Alexis Lucattini <alexis.lucattini@umccr.org>
2020-09-25 16:17:26 +1000

    Run check on cluster scripts to ensure that the user has logged in to AWS

--- parallel_cluster/bin/list_clusters.sh
+++ parallel_cluster/bin/list_clusters.sh
@@ -1,11 +1,30 @@
 #!/usr/bin/env bash
 
+# Functions
+echo_stderr(){
+    # Write log to stderr
+    echo "${@}" 1>&2
+}
+
+has_creds(){
+  : '
+  Are credentials present on CLI
+  '
+  aws sts get-caller-identity >/dev/null 2>&1
+  return "$?"
+}
+
 # Check version
 check_pcluster_version() {
   # Used to ensure that pcluster is in our PATH
   pcluster version >/dev/null 2>&1
   return "$?"
 }
+
+if ! has_creds; then
+    echo_stderr "Could not find credentials, please login to AWS before continuing"
+    exit 1
+fi
 
 if ! check_pcluster_version; then
   echo_stderr "Could not get version, from command 'pcluster version'."

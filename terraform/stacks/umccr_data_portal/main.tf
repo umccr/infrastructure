6f36f64ca8c0d0bb3908f93ac1f0065e7e3cd45a
Roman Valls Guimera <brainstorm@nopcode.org>
2021-02-02 15:42:55 +1100

    Bump github provider too

--- terraform/stacks/umccr_data_portal/main.tf
+++ terraform/stacks/umccr_data_portal/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 0.14"
 
   backend "s3" {
     bucket         = "umccr-terraform-states"
@@ -15,7 +15,8 @@ terraform {
     }
 
     github = {
-      version = "~> 3.0.0"
+      version = "~> 4.3.1"
+      source = "integrations/github"
     }
   }
 }

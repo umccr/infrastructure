a092144297b4e03d0147dc2fa40c64f741aac915
Roman Valls Guimera <brainstorm@nopcode.org>
2021-05-25 16:11:29 +1000

    Simplify and finish proof of concept to the max, just consume event from Illumina GDS landing on our SQS queue and json.dumps it into cloudwatch

--- cdk/apps/thebus/stacks/thebus_stack.py
+++ cdk/apps/thebus/stacks/thebus_stack.py
@@ -3,6 +3,8 @@ from aws_cdk import Stack
 from aws_cdk import aws_events as events
 from aws_cdk import aws_events_targets as targets
 from aws_cdk import (
+    Duration,
+    aws_lambda as _lambda,
     aws_sqs as sqs,
     aws_iam as iam
 )
@@ -12,30 +14,40 @@ class TheBusStack(Stack):
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # 1) Create GDS SQS queue for Illumina file events
-        gds_events_queue = sqs.Queue(self, id="umccr-bus-dev-iap-ens-event-queue") \
+        # Creates GDS SQS queue for Illumina file events
+        _ = sqs.Queue(self, id="umccr-bus-dev-iap-ens-event-queue") \
                               .grant_send_messages(iam.AccountPrincipal('079623148045'))
+        lmbda_handler = TheBusStack.lambdaDeploy(self)
+        eventBus, eventPattern, lmbda_target = TheBusStack.createEventBusAndEventPatternForSQSAndLambdaTarget(self, lmbda_handler)
+        TheBusStack.createRule(self, [lmbda_target], eventBus, eventPattern)
 
-        # 2) prepare resources EventBus, EventPattern, Rule-Target
-        eventBus, eventPattern = TheBusStack.createEventBusAndEventPatternForSQS(self, gds_events_queue)
-
-        # 3) create rule using resources: EventBus, EventPattern, Rule-Targets: Lambda and State-Machine
-        TheBusStack.createRule(self, eventBus, eventPattern)
-
-    # -----------------------------------------------------------------------------------
     @staticmethod
-    def createEventBusAndEventPatternForSQS(this, queue: sqs.Queue):
+    def lambdaDeploy(this) -> None:
+        pipeline = _lambda.Function(this, 'reports_ingestor',
+                                       function_name='reports_ingestor',
+                                       handler='reports.handler',
+                                       runtime=_lambda.Runtime.PYTHON_3_8,
+                                       code=_lambda.Code.from_asset('lambdas'),
+                                       timeout=Duration.seconds(20),
+                                    )
+        return pipeline
+
+    @staticmethod
+    def createEventBusAndEventPatternForSQSAndLambdaTarget(this, lmbda_target: _lambda.IFunction):
         eventBus      = events.EventBus(scope=this, id="umccr_bus", event_bus_name="umccr_bus")
         eventPattern  = events.EventPattern(source=['aws.sqs'])
+        lambdaTarget1 = targets.LambdaFunction(handler = lmbda_target)
 
-        return eventBus, eventPattern
-    # -----------------------------------------------------------------------------------
+        return eventBus, eventPattern, lambdaTarget1
+
     @staticmethod
-    def createRule(this, eventBus: typing.Optional["IEventBus"]=None, 
+    def createRule(this, targetsList : typing.Optional[typing.List["IRuleTarget"]] = None,
+                         eventBus: typing.Optional["IEventBus"]=None, 
                          eventPattern: typing.Optional["EventPattern"]=None):
         events.Rule(scope=this,
                     id="gds_file_events_trigger",
                     rule_name="gds_files_events",
+                    targets=targetsList,
                     description="Rule",
                     event_bus=eventBus,
                     event_pattern=eventPattern,

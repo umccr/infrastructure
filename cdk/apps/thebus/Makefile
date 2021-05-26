dce6e7ec673904869acbbbe8f5819be15e2512d8
Roman Valls Guimera <brainstorm@nopcode.org>
2021-05-26 10:35:00 +1000

    Adress most of @reisingerf feedback, refer to event payload transformers next

--- cdk/apps/thebus/Makefile
+++ cdk/apps/thebus/Makefile
@@ -1,6 +1,6 @@
 build:
 	sam-beta-cdk build
 deploy: build
-	cdk deploy -a .aws-sam/build --profile dev
+	cdk deploy -a .aws-sam/build --profile ${AWS_PROFILE}
 run:
 	sam-beta-cdk local invoke
--- cdk/apps/thebus/stacks/thebus_stack.py
+++ cdk/apps/thebus/stacks/thebus_stack.py
@@ -18,27 +18,33 @@ class TheBusStack(Stack):
         _ = sqs.Queue(self, id="umccr-bus-dev-iap-ens-event-queue") \
                               .grant_send_messages(iam.AccountPrincipal('079623148045'))
         lmbda_handler = TheBusStack.lambdaDeploy(self)
-        eventBus, eventPattern, lmbda_target = TheBusStack.createEventBusAndEventPatternForSQSAndLambdaTarget(self, lmbda_handler)
+        eventBus = TheBusStack.createEventBus(self)
+        eventPattern = TheBusStack.setEventPatternForSQS()
+        lmbda_target = TheBusStack.setLambdaTarget(self, lmbda_handler)
         TheBusStack.createRule(self, [lmbda_target], eventBus, eventPattern)
 
     @staticmethod
     def lambdaDeploy(this) -> None:
-        pipeline = _lambda.Function(this, 'reports_ingestor',
-                                       function_name='reports_ingestor',
-                                       handler='reports.handler',
+        pipeline = _lambda.Function(this, 'gds',
+                                       function_name='gds',
+                                       handler='gds.lambda_handler',
                                        runtime=_lambda.Runtime.PYTHON_3_8,
-                                       code=_lambda.Code.from_asset('lambdas'),
+                                       code=_lambda.Code.from_asset('lambdas/gds'),
                                        timeout=Duration.seconds(20),
                                     )
         return pipeline
 
     @staticmethod
-    def createEventBusAndEventPatternForSQSAndLambdaTarget(this, lmbda_target: _lambda.IFunction):
-        eventBus      = events.EventBus(scope=this, id="umccr_bus", event_bus_name="umccr_bus")
-        eventPattern  = events.EventPattern(source=['aws.sqs'])
-        lambdaTarget1 = targets.LambdaFunction(handler = lmbda_target)
+    def createEventBus(this):
+        return events.EventBus(scope=this, id="umccr_bus", event_bus_name="umccr_bus")
 
-        return eventBus, eventPattern, lambdaTarget1
+    @staticmethod
+    def setEventPatternForSQS():
+        return events.EventPattern(source=['aws.sqs'])
+    
+    @staticmethod
+    def setLambdaTarget(self, lmbda_target: _lambda.IFunction):
+        return targets.LambdaFunction(handler = lmbda_target)
 
     @staticmethod
     def createRule(this, targetsList : typing.Optional[typing.List["IRuleTarget"]] = None,

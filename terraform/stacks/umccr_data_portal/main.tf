a56f11495a1d32f6555674753d78db496a6829a2
Alexis Lucattini <alexis.lucattini@umccr.org>
2021-07-07 14:52:55 +1000

    Added the sqs queue for ct tso runs

--- terraform/stacks/umccr_data_portal/main.tf
+++ terraform/stacks/umccr_data_portal/main.tf
@@ -332,6 +332,14 @@ resource "aws_sqs_queue" "dragen_wgs_qc_queue" {
   tags = merge(local.default_tags)
 }
 
+resource "aws_sqs_queue" "dragen_tso_ctdna_queue" {
+  name = "${local.stack_name_dash}-dragen-tso-ctdna-queue.fifo"
+  fifo_queue = true
+  content_based_deduplication = true
+  visibility_timeout_seconds = 30*6  # lambda function timeout * 6
+  tags = merge(local.default_tags)
+}
+
 resource "aws_sqs_queue" "tn_queue" {
   name = "${local.stack_name_dash}-tn-queue.fifo"
   fifo_queue = true
@@ -1552,6 +1560,13 @@ resource "aws_ssm_parameter" "sqs_dragen_wgs_qc_queue_arn" {
   name  = "${local.ssm_param_key_backend_prefix}/sqs_dragen_wgs_qc_queue_arn"
   type  = "String"
   value = aws_sqs_queue.dragen_wgs_qc_queue.arn
+  tags  = merge(local.default_tags)
+}
+
+resource "aws_ssm_parameter" "sqs_dragen_tso_ctdna_queue_arn" {
+  name  = "${local.ssm_param_key_backend_prefix}/sqs_dragen_tso_ctdna_queue_arn"
+  type  = "String"
+  value = aws_sqs_queue.dragen_tso_ctdna_queue.arn
   tags  = merge(local.default_tags)
 }
 

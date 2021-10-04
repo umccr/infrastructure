ee7496b1551254ce2bed562d846339b6ca8cad5d
Victor San Kho Lin <victor@sankholin.com>
2021-10-04 15:42:22 +1100

    Portal TF: Bump `aws/codebuild/standard:5.0`
    * Prep for next LTS: Python3.9 and Node 14
    * See https://docs.aws.amazon.com/codebuild/latest/userguide/available-runtimes.html

--- terraform/stacks/umccr_data_portal/cicd.tf
+++ terraform/stacks/umccr_data_portal/cicd.tf
@@ -212,7 +212,7 @@ resource "aws_codebuild_project" "codebuild_client" {
 
   environment {
     compute_type = "BUILD_GENERAL1_SMALL"
-    image        = "aws/codebuild/standard:4.0"
+    image        = "aws/codebuild/standard:5.0"
     type         = "LINUX_CONTAINER"
 
     environment_variable {
@@ -291,7 +291,7 @@ resource "aws_codebuild_project" "codebuild_apis" {
 
   environment {
     compute_type    = "BUILD_GENERAL1_SMALL"
-    image           = "aws/codebuild/standard:4.0"
+    image           = "aws/codebuild/standard:5.0"
     type            = "LINUX_CONTAINER"
     privileged_mode = true
 

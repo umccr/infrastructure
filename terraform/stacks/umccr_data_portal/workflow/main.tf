30b36b325641c4d7402178220c39004819c399ba
Alexis Lucattini <alexis.lucattini@umccr.org>
2021-11-08 12:35:48 +1100

    Updated cttso prod workflow version to include CombinedVariantOutput

--- terraform/stacks/umccr_data_portal/workflow/main.tf
+++ terraform/stacks/umccr_data_portal/workflow/main.tf
@@ -284,7 +284,7 @@ locals {
 
   dragen_tso_ctdna_wfl_version = {
     dev = "1.1.0--1.0.0"
-    prod = "1.1.0--1.0.0--570feb0"
+    prod = "1.1.0--1.0.0--a303dbc"
   }
 
   dragen_tso_ctdna_wfl_input = {

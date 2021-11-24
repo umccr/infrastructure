1b0c0675ad1ab78194c2fe52f900f35cf68088a4
Alexis Lucattini <alexis.lucattini@umccr.org>
2021-11-24 12:13:39 +1100

    Add prod compatibility to update-params command

--- cdk/apps/cttso-ica-to-pieriandx/update-params.sh
+++ cdk/apps/cttso-ica-to-pieriandx/update-params.sh
@@ -3,13 +3,35 @@
 # Set to fail
 set -euo pipefail
 
+# Get json file based on account
+account_id="$(aws sts get-caller-identity | jq --raw-output '.Account')"
+
+if [[ "${account_id}" == "" ]]; then
+  echo "Deploying in dev" 1>&2
+  params_file="params-dev.json"
+elif [[ "${account_id}" == "472057503814" ]]; then
+  echo "Deploying in prod" 1>&2
+  params_file="params-prod.json"
+else
+  echo "Could not get params file, please ensure you're logged in to either dev or prod" 1>&2
+  exit 1
+fi
+
+# Check json file exists
+if [[ ! -f "${params_file}" ]]; then
+  echo "Error, could not find file '${params_file}'. Exiting" 1>&2
+fi
+
 # Get keys
-param_keys="$(jq --raw-output 'keys[]' < params-dev.json)"
+param_keys="$(jq --raw-output 'keys[]' < "${params_file}")"
 
 # Iterate through keys in for loop
 for key in ${param_keys}; do
   # Get value
-  value="$(jq --raw-output --arg key_name "${key}" '.[$key_name]' < params-dev.json)"
+  value="$(jq \
+             --raw-output \
+             --arg key_name "${key}" '.[$key_name]' \
+           < "${params_file}")"
 
   if aws ssm get-parameter --name "${key}" 1>/dev/null 2>&1; then
     # Get current value in aws ssm

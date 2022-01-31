9608d3e8016454ceccdfe0758475ca591e95084d
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2022-01-31 13:49:19 +1100

    AGHA TF: bucket name update for user permissions

--- terraform/stacks/agha_users/variables.tf
+++ terraform/stacks/agha_users/variables.tf
@@ -6,11 +6,11 @@ variable "stack_name" {
 # S3 buckets
 # (need to be the same as defined in the agha_infra stack)
 variable "agha_gdr_staging_bucket_name" {
-  default = "agha-gdr-staging"
+  default = "agha-gdr-staging-2.0"
 }
 
 variable "agha_gdr_store_bucket_name" {
-  default = "agha-gdr-store"
+  default = "agha-gdr-store-2.0"
 }
 
 variable "agha_gdr_mm_bucket_name" {

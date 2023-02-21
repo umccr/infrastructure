7aa4e2ef3fe3ed44ff251f8856d0298d32cbbbd4
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2023-02-21 15:29:41 +1100

    Not fifo anymore here either...

--- cdk/apps/dracarys/lambda/test/test_queue_msg.sh
+++ cdk/apps/dracarys/lambda/test/test_queue_msg.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 aws sqs send-message --region ap-southeast-2 --endpoint-url https://sqs.ap-southeast-2.amazonaws.com/ \
-                                             --queue-url https://sqs.ap-southeast-2.amazonaws.com/843407916570/data-portal-dracarys-queue.fifo \
+                                             --queue-url https://sqs.ap-southeast-2.amazonaws.com/843407916570/data-portal-dracarys-queue \
                                              --message-body "This is a Dracarys test" \
                                              --message-attributes file://test_queue_msg.json

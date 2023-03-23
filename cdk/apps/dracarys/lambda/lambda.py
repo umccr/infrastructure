e9bb7a89097fb3b270d3b3582179b74b9645124f
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2023-03-23 17:03:04 +1100

    Generating all tidied multiqc data and exporting to S3 datalake bucket for many subjects. Parsing portal_run_id and subject_id from the GDS_URL for now :shrug:

--- cdk/apps/dracarys/lambda/lambda.py
+++ cdk/apps/dracarys/lambda/lambda.py
@@ -1,5 +1,6 @@
 import json
 import os
+import re
 import boto3
 import datetime
 import base64
@@ -7,6 +8,9 @@ import logging
 import subprocess
 
 def handler(event, context):
+    #now = datetime.datetime.now().strftime("%Y-%m-%d")
+    s3 = boto3.resource('s3')
+
     logging.info('request: {}'.format(json.dumps(event)))
     print('request: {}'.format(json.dumps(event)))
     msg_attrs = event['Records'][0]['messageAttributes']
@@ -33,14 +37,24 @@ def handler(event, context):
     CWD = "/tmp/dracarys"
     os.makedirs(CWD, exist_ok=True)
 
-    # TODO: Add 3 more sample inputs
-    output = run_command(["conda","run","-n","dracarys_env","/bin/bash","-c","dracarys.R tidy -i " + gds_input + " -o " + CWD + " -p " + file_prefix, "--portal-run-id", portal_run_id])
+    gds_path_data = parse_gds_path_info(gds_input)
+    assert(gds_path_data != None)
 
-    s3 = boto3.resource('s3')
-    target_prefix = DATA_ENV+"/creation_date="+datetime.datetime.now().strftime("%Y-%m-%d")+"/"
-    target_fname = "dracarys_multiqc.tsv.gz"
+    # Forced to parse portal_run_id from GDS URL for now... 
+    #output = run_command(["conda","run","-n","dracarys_env","/bin/bash","-c","dracarys.R tidy -i " + gds_input + " -o " + CWD + " -p " + file_prefix, "--portal-run-id", portal_run_id])
+    output = run_command(["conda","run","-n","dracarys_env","/bin/bash","-c","dracarys.R tidy -i " + gds_input + " -o " + CWD + " -p " + file_prefix, " -f both"])
+
+    # Write in both TSV and Parquet formats
+    target_fname = "multiqc_data.parquet"
     target_fname_path = find(target_fname, CWD)
+    target_prefix = DATA_ENV+"/creation_date="+ gds_path_data['portal_run_id_date'] + "/subject_id="+ gds_path_data['sbj_id'] +" /format=parquet"
     s3.meta.client.upload_file(target_fname_path, target_bucket_name, os.path.join(target_prefix, target_fname))
+
+    target_fname = "multiqc_data.tsv.gz"
+    target_fname_path = find(target_fname, CWD)
+    target_prefix = DATA_ENV+"/creation_date="+ gds_path_data['portal_run_id_date'] + "/subject_id="+ gds_path_data['sbj_id'] +" /format=tsv"
+    s3.meta.client.upload_file(target_fname_path, target_bucket_name, os.path.join(target_prefix, target_fname))
+
     returnmessage = ('Wrote ' + str(target_fname) + ' to s3://' + target_bucket_name )
 
     logging.info(returnmessage)
@@ -51,6 +65,25 @@ def handler(event, context):
         },
         'body':  (returnmessage ) 
     }
+
+def parse_gds_path_info(gds_url: str) -> dict | None:
+    ''' A portal run id (20230311b504283e) is a string composed of
+        a datetime 20230311 and a UUID/hash: b504283e 
+    '''
+    #                                                      SBJID   PORTAL_RUN_ID_DATE+HASH  MULTIQC_DIR
+    gds_url_regex = r'/gds:\/\/production\/analysis_data\/(\w+)\/\w+\/(\d{8})(\w+)\/\w+\/(\w+)\//'
+    match = re.search(gds_url_regex, gds_url)
+    components = dict()
+
+    if match:
+        components['sbj_id'] = match.group(1)
+        components['portal_run_id_date'] = match.group(2)
+        components['portal_run_id_hash'] = match.group(3)
+        components['multiqc_dir'] = match.group(4)
+
+        return components
+    else:
+        return None
 
 def find(name, path):
     for root, _, files in os.walk(path):
@@ -66,7 +99,6 @@ def run_command(args):
 
     output, error = p.communicate() 
     logging.info("the commandline is {}".format(p.args))
-    #getcommand(p)
     logging.info("the return code is " + str(p.returncode))
     if p.returncode == 0: 
         print('output :\n {0}'.format(output.decode("utf-8"))) 

e5818380a863440aba6a4403ce8f1eed9ee38711
Roman Valls Guimera <brainstorm@users.noreply.github.com>
2023-04-27 16:55:21 +1000

    Eliminate the precursor, PoC, to OrcaBus (#304)

--- cdk/apps/thebus/lambdas/layers/schemas/schema/weslaunchrequest/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-
-from __future__ import absolute_import
-
-from schema.weslaunchrequest.marshaller import Marshaller
-from schema.weslaunchrequest.Event import Event
--- cdk/apps/thebus/lambdas/layers/schemas/schema/workflowrequest/__init__.py
+++
@@ -1,6 +0,0 @@
-# coding: utf-8
-
-from __future__ import absolute_import
-
-from schema.workflowrequest.marshaller import Marshaller
-from schema.workflowrequest.Event import Event

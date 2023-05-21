1c2131013aa5c2fd92011276572b0ebf3e6d35bd
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2023-05-22 09:52:56 +1000

    Update variables.tf with oncoanalyser bucket name

--- terraform/stacks/data_archive/variables.tf
+++ terraform/stacks/data_archive/variables.tf
@@ -2,3 +2,8 @@ variable "raw_data_bucket_name" {
   description = "The name of the bucket for raw sequencer output (BCLs)."
   default     = "org.umccr.data.raw-data-archive"
 }
+
+variable "oncoanalyser_bucket_name" {
+  description = "The name of the bucket for raw sequencer output (BCLs)."
+  default     = "org.umccr.data.oncoanalyser"
+}

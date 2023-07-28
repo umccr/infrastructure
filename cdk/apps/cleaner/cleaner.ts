ac941caccbe7fbf0ce4721ce1dd85ad157eaf4bc
andrewpatto <andrew@patto.net>
2023-07-28 14:23:56 +1000

    Tidy Setup for dev and build

--- cdk/apps/cleaner/cleaner.ts
+++ cdk/apps/cleaner/cleaner.ts
@@ -2,14 +2,30 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { CleanerStack } from "./lib/cleaner-stack";
+import {Duration} from "aws-cdk-lib";
 
 const description = "Cleaner is a service that deletes unused CDK repos and S3 assets each night";
 
 const app = new cdk.App();
-new CleanerStack(app, "CleanerStack", {
+
+// dev
+new CleanerStack(app, "CleanerDevStack", {
   description: description,
   env: {
     account: "843407916570",
     region: "ap-southeast-2"
   },
+  // do not delete assets created in the last 7 days even if unused
+  retainAssetsNewerThan: Duration.days(7),
+});
+
+// build (bastion)
+new CleanerStack(app, "CleanerBuildStack", {
+  description: description,
+  env: {
+    account: "383856791668",
+    region: "ap-southeast-2"
+  },
+  // do not delete assets created in the last 30 days even if unused
+  retainAssetsNewerThan: Duration.days(30),
 });

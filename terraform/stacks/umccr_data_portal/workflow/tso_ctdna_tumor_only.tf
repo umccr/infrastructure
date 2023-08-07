ed2ae8837f5ae20a7dfdc1aeea7a5c9a2c429877
Alexis Lucattini <Alexis.Lucattini@umccr.org>
2023-08-07 22:31:38 +1000

    Updated tso dna workflow run template to version 1.2 (prod)

--- terraform/stacks/umccr_data_portal/workflow/tso_ctdna_tumor_only.tf
+++ terraform/stacks/umccr_data_portal/workflow/tso_ctdna_tumor_only.tf
@@ -7,7 +7,7 @@ locals {
 
   tso_ctdna_tumor_only_wfl_version = {
     dev  = "1.2.0--1.0.0"
-    prod = "1.1.0--1.0.0--d7621f6"
+    prod = "1.2.0--1.0.0--3473aed"
     stg  = "1.2.0--1.0.0--3473aed"
   }
 

5b7754db474488cdebbe8b9dcfcb6236725d5aca
Andrew Patterson <andrew@patto.net>
2023-10-04 13:44:06 +1100

    Update variables.tf

--- terraform/stacks/umccr_demo_admin/variables.tf
+++ terraform/stacks/umccr_demo_admin/variables.tf
@@ -1,5 +1,6 @@
 variable "elsa_data_data_bucket_paths" {
   default = {
+    org.umccr.demo.elsa-data-demo-data = ["*"]
     umccr-10f-data-dev = ["ASHKENAZIM/*", "CHINESE/*"]
     umccr-10g-data-dev = ["*"]
   }

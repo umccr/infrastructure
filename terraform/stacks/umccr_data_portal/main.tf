696ac6873624cf9ee55cec1ee93368cf95b7f2e0
Victor San Kho Lin <victor@sankholin.com>
2023-10-18 19:11:48 +1100

    Portal TF: Bumped terraform 1.5.7 & aws provider 5.21.0

--- terraform/stacks/umccr_data_portal/main.tf
+++ terraform/stacks/umccr_data_portal/main.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = ">= 1.3.3"
+  required_version = ">= 1.5.7"
 
   backend "s3" {
     bucket         = "umccr-terraform-states"
@@ -11,7 +11,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.38.0"
+      version = "5.21.0"
     }
   }
 }

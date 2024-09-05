953047f03092bef4c076247cd95bd3ce3714274d
william <william.intan@unimelb.edu.au>
2024-09-05 12:03:23 +1000

    Update byob_ica_v2.tf

--- terraform/stacks/unimelb/data_archive/byob_ica_v2.tf
+++ terraform/stacks/unimelb/data_archive/byob_ica_v2.tf
@@ -176,8 +176,9 @@ resource "aws_s3_bucket_cors_configuration" "production_data" {
     allowed_headers = ["*"]
     allowed_methods = ["HEAD", "GET", "PUT", "POST", "DELETE"]
     allowed_origins = [
-      "https://ica.illumina.com", # ILMN UI uploads - https://help.ica.illumina.com/home/h-storage/s-awss3
-      "https://orcaui.umccr.org", # orcaui - https://github.com/umccr/orca-ui
+      "https://ica.illumina.com",       # ILMN UI uploads - https://help.ica.illumina.com/home/h-storage/s-awss3
+      "https://orcaui.umccr.org",       # orcaui - https://github.com/umccr/orca-ui
+      "https://orcaui.prod.umccr.org",  # orcaui - https://github.com/umccr/orca-ui
     ]
     expose_headers  = ["ETag", "x-amz-meta-custom-header"]
     max_age_seconds = 3000

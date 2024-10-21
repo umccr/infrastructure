5daeb42945db9b7b5c5fb0665732e4bac73cbb26
Victor San Kho Lin <victor@sankholin.com>
2024-10-21 18:24:07 +1100

    Bootstrap TF: Added deprecation note to terraform bootstrap stack
    * All buckets and related policy have been decoupled from this stack.
      All terraform workspaces and their states have been wiped.

--- terraform/stacks/bootstrap/outputs.tf
+++ terraform/stacks/bootstrap/outputs.tf
@@ -1,19 +1,19 @@
-output "workspace" {
-  value = "${terraform.workspace}"
-}
-
-output "slack_notify_lambda_arn" {
-  value = "${module.notify_slack_lambda.function_arn}"
-}
-
-output "presigned_urls_username" {
-  value = "${module.presigned_urls.username}"
-}
-
-output "presigned_urls_access_key" {
-  value = "${module.presigned_urls.access_key}"
-}
-
-output "presigned_urls_access_secret_key" {
-  value = "${module.presigned_urls.encrypted_secret_access_key}"
-}
+# output "workspace" {
+#   value = "${terraform.workspace}"
+# }
+#
+# output "slack_notify_lambda_arn" {
+#   value = "${module.notify_slack_lambda.function_arn}"
+# }
+#
+# output "presigned_urls_username" {
+#   value = "${module.presigned_urls.username}"
+# }
+#
+# output "presigned_urls_access_key" {
+#   value = "${module.presigned_urls.access_key}"
+# }
+#
+# output "presigned_urls_access_secret_key" {
+#   value = "${module.presigned_urls.encrypted_secret_access_key}"
+# }

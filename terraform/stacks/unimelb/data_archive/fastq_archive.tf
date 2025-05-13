24cb54081b2b39ad5c63b07f4e29b86d7664786b
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2025-05-13 14:33:57 +1000

    Add FileManager pre-sign access statement to fastq archive bucket

--- terraform/stacks/unimelb/data_archive/fastq_archive.tf
+++ terraform/stacks/unimelb/data_archive/fastq_archive.tf
@@ -123,6 +123,22 @@ data "aws_iam_policy_document" "fastq_archive" {
     ])
   }
 
+  statement {
+    sid = "orcabus_file_manager_presign_access"
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${local.account_id_prod}:user/${local.orcabus_file_manager_presign_user}"]
+    }
+    actions = sort([
+      "s3:ListBucket",
+      "s3:GetObject",
+    ])
+    resources = sort([
+      aws_s3_bucket.fastq_archive.arn,
+      "${aws_s3_bucket.fastq_archive.arn}/*",
+    ])
+  }
+
   # Allow the data mover access to copy to this bucket.
   statement {
     sid = "orcabus_data_mover_access"

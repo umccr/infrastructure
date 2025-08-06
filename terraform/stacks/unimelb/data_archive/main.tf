771934e4cd3934471075260ead92db6cdc570649
Florian Reisinger <florian.reisinger@unimelb.edu.au>
2025-08-06 14:43:42 +1000

    Add atlas BYOB to ICA IAM user permissions

--- terraform/stacks/unimelb/data_archive/main.tf
+++ terraform/stacks/unimelb/data_archive/main.tf
@@ -23,14 +23,15 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 
 locals {
-  region          = "ap-southeast-2"
-  stack_name      = "data_archive"
-  this_account_id = data.aws_caller_identity.current.account_id
-  mgmt_account_id = "363226301494"
-  account_id_prod = "472057503814"
-  account_id_stg  = "455634345446"
-  account_id_dev  = "843407916570"
-  account_id_org  = "650704067584"
+  region           = "ap-southeast-2"
+  stack_name       = "data_archive"
+  this_account_id  = data.aws_caller_identity.current.account_id
+  mgmt_account_id  = "363226301494"
+  account_id_prod  = "472057503814"
+  account_id_stg   = "455634345446"
+  account_id_dev   = "843407916570"
+  account_id_org   = "650704067584"
+  account_id_atlas = "550435500918"
 
   default_tags = {
     "Stack"       = local.stack_name

3bd2f946cb2a452e7e3ed85f436d0c6150197035
william <william.intan@unimelb.edu.au>
2025-11-26 17:20:56 +1100

    Update main.tf

--- terraform/stacks/umccr/security/main.tf
+++ terraform/stacks/umccr/security/main.tf
@@ -257,6 +257,7 @@ resource "aws_cloudwatch_event_rule" "security_event_routing" {
     detail-type = ["Access Analyzer Finding"]
     detail = {
       isDeleted = [false]
+      status = ["ACTIVE"]
     }
   })
 }
@@ -277,6 +278,7 @@ resource "aws_cloudwatch_event_target" "security_event_routing" {
       "principal" : "$.detail.principal.AWS",
       "resource" : "$.detail.resource",
       "resourceType" : "$.detail.resourceType"
+      "status" : "$.detail.status"
     }
 
     input_template = <<EOT
@@ -285,12 +287,14 @@ resource "aws_cloudwatch_event_target" "security_event_routing" {
   "source": "custom",
   "content": {
     "textType": "client-markdown",
-    "title": "Access Analyzer Finding - UMCCR (<eventAccountId>)",
-    "description": "*CONTEXT* \n• Finding ID: `<id>`\n\n*RESOURCE*\n• Account ID: `<accountId>`\n• Type: `<resourceType>`\n• ARN: `<resource>`\n\n*ACCESS DETAILS*\n• Principal (AWS Account): `<principal>`"
+    "title": "Access Analyzer <status> Finding - Reporting Account: <eventAccountId>",
+    "description": "Finding ID: `<id>`\n\n*Resource*\n• Account ID: `<accountId>`\n• Type: `<resourceType>`\n• ARN: `<resource>`\n\n*Access Details*\n• Principal (AWS Account): `<principal>`"
   }
 }
 EOT
   }
 
 }
+
+
 
